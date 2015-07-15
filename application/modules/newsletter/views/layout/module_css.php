992204b590a42763cb001195d8bf024265a6f0de
Matteo Puppis <matteo@h2-web.it>
2015-07-15 15:15:27 +0200

    Beta release. Da oggi Alberto inizia a lavorare sul multilingua, quindi ci sarà una bella major release!

--- application/modules/newsletter/views/layout/module_css.php
+++
@@ -1,3 +0,0 @@
-<!-- BEGIN Module Related CSS -->
-<link href="<?php echo base_url_template("template/crm/css/pages/inbox.css"); ?>" rel="stylesheet" type="text/css" />
-<!-- END Module Related CSS -->
