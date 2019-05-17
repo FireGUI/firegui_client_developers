b9304ef49aca150c8d5c9f5a12aa21716120646a
matteo <web@idra.h24hosting.com>
2019-05-17 15:51:15 +0200

    Added pullFromGit function

--- application/controllers/Firegui.php
+++ application/controllers/Firegui.php
@@ -3,51 +3,72 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Firegui extends MY_Controller {
-    
+class Firegui extends MY_Controller
+{
+
     function __construct()
     {
-        parent :: __construct();
+        parent:: __construct();
 
         // TODO: INTEGRARE SISTEMA DI PROTEZIONE, SOLO FIREGUI DEVE POTER ESEGUIRE QUESTI METODI SE QUALCUNO SCOPRE
     }
 
-    public function createModule($identifier) {
+    public function createModule($identifier)
+    {
 
         //Creo le cartelle necessarie
         $folders = [
             'controllers', 'models', 'views', 'assets'
         ];
 
-        $prefix_folder = FCPATH.'application/modules/'.$identifier.'/';
+        $prefix_folder = FCPATH . 'application/modules/' . $identifier . '/';
 
         foreach ($folders as $folder) {
-            if(!is_dir($prefix_folder.$folder)) //create the folder if it's not already exists
+            if (!is_dir($prefix_folder . $folder)) //create the folder if it's not already exists
             {
-                mkdir($prefix_folder.$folder, DIR_WRITE_MODE, true);
+                mkdir($prefix_folder . $folder, DIR_WRITE_MODE, true);
             }
         }
 
     }
 
-    public function uninstallModule($identifier) {
+    public function uninstallModule($identifier)
+    {
         // TODO: Pericoloso esporre un metodo del genere
-        unlink(FCPATH."application/modules/$identifier/");
+        unlink(FCPATH . "application/modules/$identifier/");
     }
-    
-    public function downloadModuleFolder($identifier) {
+
+    function updateFromGit($command = null, $output = true)
+    {
+
+        if ($command == null) {
+            $command = "git pull https://github.com/FireGUI/firegui_client_developers.git master";
+        }
+
+        $result = array();
+        exec($command, $result);
+
+        if ($output == true) {
+            foreach ($result as $line) {
+                print($line . "\n");
+            }
+        }
+    }
+
+    public function downloadModuleFolder($identifier)
+    {
         $module = $this->db->where('modules_identifier', $identifier)->get('modules')->row_array();
 
         if (!$module) {
             die('Unauthorized');
         } else {
-            
-            $folder = FCPATH.'application/modules/'.$identifier.'/';
 
-            $destination_file = FCPATH.'uploads/'.$identifier.'.zip';
+            $folder = FCPATH . 'application/modules/' . $identifier . '/';
+
+            $destination_file = FCPATH . 'uploads/' . $identifier . '.zip';
 
             if (!file_exists($folder)) {
-                die('Can not create module, folder not found: '.$destination_file);
+                die('Can not create module, folder not found: ' . $destination_file);
             }
 
 
@@ -56,16 +77,16 @@ class Firegui extends MY_Controller {
             if ($success === TRUE) { //NON TORNA FALSE!!!!!!
                 if (file_exists($destination_file)) {
                     header('Content-Type: application/zip');
-                    header('Content-Disposition: attachment; filename="'.$identifier.'.zip"');
-                    header('Content-Length: '.filesize($destination_file) );
+                    header('Content-Disposition: attachment; filename="' . $identifier . '.zip"');
+                    header('Content-Length: ' . filesize($destination_file));
                     //die(filesize($destination_file));
                     //ob_end_clean();
                     readfile($destination_file);
                     //unlink($destination_file);
                 } else {
-                    die('Can not create  '.$destination_file);
+                    die('Can not create  ' . $destination_file);
                 }
-                
+
             } else {
                 die($success);
             }
@@ -73,33 +94,31 @@ class Firegui extends MY_Controller {
     }
 
 
-
-
-    public function uploadModule($identifier) {
+    public function uploadModule($identifier)
+    {
         $zip = new ZipArchive;
- 
-        if ($zip->open($_FILES['module_file']['tmp_name']) === TRUE) 
-        {
-            $zip->extractTo(FCPATH."/application/modules/{$identifier}");
+
+        if ($zip->open($_FILES['module_file']['tmp_name']) === TRUE) {
+            $zip->extractTo(FCPATH . "/application/modules/{$identifier}");
             $zip->close();
         } else {
             die('Wrong zip archive!');
         }
-        
+
         debug($_FILES);
-        
+
         if (file_exists($_FILES['module_file']['tmp_name'])) {
             echo('file esistente');
         } else {
-            
+
         }
-        
+
         $content = file_get_contents($_FILES['module_file']['tmp_name']);
         echo('Inizio contenuto...');
         echo(strlen($content));
         //echo($content);
         die('test');
-        
+
     }
 
 
