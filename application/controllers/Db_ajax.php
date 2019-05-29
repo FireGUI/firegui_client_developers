44a6a9114f65cf507e3a76eadce50684a214f5ab
matteo <web@idra.h24hosting.com>
2019-05-29 16:43:26 +0200

    Commit automatico

--- application/controllers/Db_ajax.php
+++ application/controllers/Db_ajax.php
@@ -864,7 +864,7 @@ class Db_ajax extends MY_Controller {
         $this->load->library('upload', array(
             'upload_path' => FCPATH . 'uploads/' . $localFolder,
             'allowed_types' => '*',
-            'max_size' => '50000',
+            'max_size' => '100000',
             'encrypt_name' => false,
             'file_name' => $filename,
         ));
