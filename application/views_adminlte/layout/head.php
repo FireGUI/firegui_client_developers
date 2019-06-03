e4c98a2ba147d8830cea9dfb7a1326464b0afb9c
matteo <web@idra.h24hosting.com>
2019-06-03 16:05:44 +0200

    Commit automatico

--- application/views_adminlte/layout/head.php
+++ application/views_adminlte/layout/head.php
@@ -95,7 +95,7 @@ $tsuffix = 'CRM';
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url_admin(CUSTOM_FAVICON); ?>-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="<?php echo base_url_admin(CUSTOM_FAVICON); ?>-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url_admin(CUSTOM_FAVICON); ?>-16x16.png">
-    <link rel="manifest" href="/manifest.json">
+    <!--<link rel="manifest" href="/manifest.json">-->
     <meta name="msapplication-TileImage" content="/<?php echo base_url_admin(CUSTOM_FAVICON); ?>-144x144.png">
     <meta name="theme-color" content="#ffffff">
 
