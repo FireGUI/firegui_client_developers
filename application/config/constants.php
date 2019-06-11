6f4bc906292e528be9f9c127ba529611b96d2601
matteo <web@idra.h24hosting.com>
2019-06-11 09:36:55 +0200

    Commit automatico

--- application/config/constants.php
+++ application/config/constants.php
@@ -14,7 +14,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 defined('SHOW_DEBUG_BACKTRACE') OR define('SHOW_DEBUG_BACKTRACE', TRUE);
 
 // Client Version
-defined('VERSION') OR define('VERSION', '1.3.4');
+defined('VERSION') OR define('VERSION', '1.3.5');
 /*
 |--------------------------------------------------------------------------
 | File and Directory Modes
