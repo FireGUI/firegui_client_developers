b2d06d4cdc4c8d662f9094b4d8b3bfa149241f6f
matteo <web@idra.h24hosting.com>
2019-08-06 15:58:38 +0200

    Commit automatico

--- application/views_metronic/layout/head.php
+++ application/views_metronic/layout/head.php
@@ -23,6 +23,7 @@ $tsuffix = 'CRM';
 <!-- CORE LEVEL STYLES -->
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/crm-v2/assets/global/plugins/font-awesome/css/font-awesome.min.css?v={$this->config->item('version')}"); ?>"/>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/crm-v2/assets/global/plugins/@fortawesome/fontawesome-free/css/all.min.css?v={$this->config->item('version')}"); ?>"/>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/crm-v2/assets/global/plugins/simple-line-icons/simple-line-icons.min.css?v={$this->config->item('version')}"); ?>"/>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/crm-v2/assets/global/plugins/bootstrap/css/bootstrap.min.css?v={$this->config->item('version')}"); ?>"/>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/crm-v2/assets/global/plugins/uniform/css/uniform.default.css?v={$this->config->item('version')}"); ?>"/>
