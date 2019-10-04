3c4ef844eafa3a70e388e04103d28a3b01149d66
matteo <web@idra.h24hosting.com>
2019-10-04 15:58:57 +0200

    Commit automatico

--- application/third_party/MX/Config.php
+++ application/third_party/MX/Config.php
@@ -37,11 +37,13 @@ class MX_Config extends CI_Config
 {	
 	public function load($file = '', $use_sections = FALSE, $fail_gracefully = FALSE, $_module = '') 
 	{
+            
 		if (in_array($file, $this->is_loaded, TRUE)) return $this->item($file);
 
 		$_module OR $_module = CI::$APP->router->fetch_module();
 		list($path, $file) = Modules::find($file, $_module, 'config/');
-		
+		//print_r($file);
+                //die($_module);
 		if ($path === FALSE)
 		{
 			parent::load($file, $use_sections, $fail_gracefully);					
--- application/third_party/MX/Controller.php
+++ application/third_party/MX/Controller.php
@@ -1,7 +1,7 @@
-<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+<?php (defined('BASEPATH')) or exit('No direct script access allowed');
 
 /** load the CI class for Modular Extensions **/
-require dirname(__FILE__).'/Base.php';
+require_once dirname(__FILE__) . '/Base.php';
 
 /**
  * Modular Extensions - HMVC
@@ -36,33 +36,27 @@ require dirname(__FILE__).'/Base.php';
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  **/
-class MX_Controller 
+
+class MX_Controller
 {
 	public $autoload = array();
-	
-	public function __construct() 
+
+	public function __construct()
 	{
-	
-            $class = str_replace(CI::$APP->config->item('controller_suffix'), '', get_class($this));
-		log_message('debug', $class." MX_Controller Initialized");
-		Modules::$registry[strtolower($class)] = $this;	
-		
+
+		$class = str_replace(CI::$APP->config->item('controller_suffix'), '', get_class($this));
+		log_message('debug', $class . " MX_Controller Initialized");
+		Modules::$registry[strtolower($class)] = $this;
+
 		/* copy a loader instance and initialize */
 		$this->load = clone load_class('Loader');
-		$this->load->initialize($this);	
-		
+		$this->load->initialize($this);
+
 		/* autoload module items */
 		$this->load->_autoloader($this->autoload);
-                
-                
-
-                
-
-
-                
 	}
-	
-	public function __get($class) 
+
+	public function __get($class)
 	{
 		return CI::$APP->$class;
 	}
