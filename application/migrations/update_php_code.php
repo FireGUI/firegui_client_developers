fce63847ce255d43885b4a7b0738576753777000
matteo <web@idra.h24hosting.com>
2019-10-07 10:11:34 +0200

    Commit automatico

--- application/migrations/update_php_code.php
+++ application/migrations/update_php_code.php
@@ -4,12 +4,16 @@
 
 $updates = array();
 
+$updates['1.6.5'] = [
+    'include' => '1.6.5_fi_events_migration.php'
+];
+
+// Updates to 1.6.4
+$updates['1.6.4'] = [
+    'include' => '1.6.4_newfields_menu.php'
+];
 
 // Updates to 1.6.1
 $updates['1.6.1'] = [
     'include' => '1.6.1_fontawesome_migration.php'
-];
-// Updates to 1.6.2
-$updates['1.6.2'] = [
-    'include' => '1.6.2_fi_events_migration.php'
 ];
--- changelog.md
+++ changelog.md
@@ -1,3 +1,3 @@
-Changelog version 1.6.3
+Changelog version 1.6.4
  
-Removed crm_exception_handler as exception handler
+New fields in entity menu
