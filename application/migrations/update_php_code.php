0f59c6f0305443d0f7421d0fe1bf4d9e7fecd6f9
matteo <web@idra.h24hosting.com>
2019-10-07 10:26:13 +0200

    Commit automatico

--- application/migrations/update_php_code.php
+++ application/migrations/update_php_code.php
@@ -8,11 +8,6 @@ $updates['1.6.5'] = [
     'include' => '1.6.5_fi_events_migration.php'
 ];
 
-// Updates to 1.6.4
-$updates['1.6.4'] = [
-    'include' => '1.6.4_newfields_menu.php'
-];
-
 // Updates to 1.6.1
 $updates['1.6.1'] = [
     'include' => '1.6.1_fontawesome_migration.php'
