bdca2283b42ea306ca05c2137cc079ff5df06b8f
matteo <web@idra.h24hosting.com>
2019-10-07 11:03:20 +0200

    Commit automatico

--- application/migrations/update_db.php
+++ application/migrations/update_db.php
@@ -5,13 +5,13 @@
 $updates = array();
 
 // Updates to 1.6.4
-// if ($this->db->dbdriver != 'postgres') {
-//     //mysql
-//     $updates['1.6.6'] = ["ALTER TABLE menu ADD COLUMN menu_type VARCHAR(250);", "ALTER TABLE menu ADD COLUMN menu_form BIGINT;"];
-// } else {
-//     //Pg
-//     $updates['1.6.6'] = ["ALTER TABLE menu ADD COLUMN menu_type VARCHAR;", "ALTER TABLE menu ADD COLUMN menu_form INT;"];
-// }
+if ($this->db->dbdriver != 'postgres') {
+    //mysql
+    $updates['1.6.6'] = ["ALTER TABLE menu ADD COLUMN menu_type VARCHAR(250);", "ALTER TABLE menu ADD COLUMN menu_form BIGINT;"];
+} else {
+    //Pg
+    $updates['1.6.6'] = ["ALTER TABLE menu ADD COLUMN menu_type VARCHAR;", "ALTER TABLE menu ADD COLUMN menu_form INT;"];
+}
 
 // Updates to 1.5.5
 $updates['1.5.5'] = ["UPDATE entity SET entity_login_entity = '" . DB_BOOL_TRUE . "' WHERE entity_name = '" . LOGIN_ENTITY . "'"];
