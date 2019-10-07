a12b50a4e5b5b3b751b9d37a6f4faa15e69b039e
matteo <web@idra.h24hosting.com>
2019-10-07 10:11:34 +0200

    Commit automatico

--- application/helpers/general_helper.php
+++ application/helpers/general_helper.php
@@ -937,3 +937,16 @@ if (!function_exists('send_telegram_log')) {
         return $result;
     }
 }
+if (!function_exists('my_version_compare')) {
+
+    function my_version_compare($v1, $v2)
+    {
+        if (is_string($v1) && is_string($v2)) {
+            return version_compare($v1, $v2);
+        } else {
+            debug($v1);
+            debug($v2, true);
+            return true;
+        }
+    }
+}
--- application/migrations/update_db.php
+++ application/migrations/update_db.php
@@ -4,6 +4,14 @@
 
 $updates = array();
 
+// Updates to 1.6.4
+if ($this->db->dbdriver != 'postgres') {
+    //mysql
+    $updates['1.6.4'] = ["ALTER TABLE menu ADD COLUMN menu_type VARCHAR(250);", "ALTER TABLE menu ADD COLUMN menu_form BIGINT;"];
+} else {
+    //Pg
+    $updates['1.6.4'] = ["ALTER TABLE menu ADD COLUMN menu_type VARCHAR;", "ALTER TABLE menu ADD COLUMN menu_form INT;"];
+}
 
 // Updates to 1.5.5
-$updates['1.5.5'] = ["UPDATE entity SET entity_login_entity = '".DB_BOOL_TRUE."' WHERE entity_name = '".LOGIN_ENTITY."'"];
+$updates['1.5.5'] = ["UPDATE entity SET entity_login_entity = '" . DB_BOOL_TRUE . "' WHERE entity_name = '" . LOGIN_ENTITY . "'"];
