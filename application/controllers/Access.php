de19ce4fc24ddcac4fce8c9a22a8a838c19a7428
matteo <web@idra.h24hosting.com>
2019-10-10 09:56:57 +0200

    Commit automatico

--- application/controllers/Access.php
+++ application/controllers/Access.php
@@ -1,115 +1,128 @@
 <?php
 
 
-class Access extends MY_Controller {
-    
+class Access extends MY_Controller
+{
+
     const SALT = 'ofuh249fh97H98UG876GHOICUYEGRF98ygdfds';
-    
+
     var $settings = NULL;
 
-    
-    function __construct() {
-        parent :: __construct();
-        
+
+    function __construct()
+    {
+        parent::__construct();
+        header('Access-Control-Allow-Origin: *');
+        @header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}"); //X-Requested-With
         $this->settings = $this->db->get('settings')->row_array();
     }
-    
-    
-    public function index() {
+
+
+    public function index()
+    {
         $this->login();
     }
-    
-    
-    
-    public function login() {
-        
-            $this->load->view('layout/login');
-        
-        
+
+
+
+    public function login()
+    {
+
+        $this->load->view('layout/login');
     }
-    
-    
-    public function recovery() {
+
+
+    public function recovery()
+    {
         $sent = $this->input->get('sent');
         $receiver = $this->input->get('receiver');
-        
-        if($sent && md5($receiver . self::SALT) !== $this->input->get('chk')) {
+
+        if ($sent && md5($receiver . self::SALT) !== $this->input->get('chk')) {
             $sent = false;
             $receiver = null;
         }
-        
+
         $this->load->view('layout/password-lost', array('sent' => $sent, 'receiver' => $receiver));
     }
-    
-    
-    public function logout() {
+
+
+    public function logout()
+    {
         $this->auth->logout();
         $this->session->sess_destroy();
-        
+
         // Imposta il log di logout
         $this->apilib->logSystemAction(Apilib::LOG_LOGOUT);
-        
+
         redirect();
     }
-    
-    
-    
-    public function login_start() {
-        
+
+
+
+    public function login_start()
+    {
+
         /** Servi la richiesta **/
         $data = $this->input->post();
-        $remember = true;//!empty($data['remember']);
+        $remember = true; //!empty($data['remember']);
         $timeout = $data['timeout'];
-        
+
         $data = $this->apilib->runDataProcessing(LOGIN_ENTITY, 'pre-login', $data);
-        
-        if(empty($data['users_users_email']) || empty($data['users_users_password'])) {
-            echo json_encode(array('status'=>0, 'txt'=>'Inserisci indirizzo e-mail e password'));
+
+        if (empty($data['users_users_email']) || empty($data['users_users_password'])) {
+            echo json_encode(array('status' => 0, 'txt' => 'Inserisci indirizzo e-mail e password'));
             die();
         }
-        
+
         $success = $this->auth->login_attempt($data['users_users_email'], $data['users_users_password'], $remember, $timeout);
-        
+
         //TODO: Matteo: aggiungere gli auto right join in modo che se un utente è associato a un'altra entità (esempio: aziende che fanno login, dipendenti seven, ecc...)
         //prenda in automatico anche i dati delle entità collegate... Buttare ovviamente anche questi dati in sessione
-        
-        
-        if($success) {
+
+
+        if ($success) {
             $redirection_url = $this->auth->fetch_intended_url();
-            
-            if (!$redirection_url OR $redirection_url == base_url()) {
+
+            if (!$redirection_url or $redirection_url == base_url()) {
                 $redirection_url = base_url('main/dashboard');
             }
-            
+
             $this->auth->reset_intended_url();
-            
+
             // Imposta il log di login
             $this->apilib->logSystemAction(Apilib::LOG_LOGIN);
-            $output = json_encode(array( 'status'=>1, 'txt'=>$redirection_url ));
+            $output = json_encode(array('status' => 1, 'txt' => $redirection_url));
             $output = $this->apilib->runDataProcessing(LOGIN_ENTITY, 'login', $output);
-            
-            echo $output;
-            
+            if ($this->input->is_ajax_request()) {
+                echo $output;
+            } else {
+                redirect($redirection_url);
+            }
         } else {
-            
+
             // Imposta il log di login
             $this->apilib->logSystemAction(Apilib::LOG_LOGIN_FAIL, ['get' => $_GET, 'post' => $_POST]);
-            echo json_encode(array( 'status'=>0, 'txt'=>'E-mail o password non corrispondenti' ));
+            if ($this->input->is_ajax_request()) {
+                echo json_encode(array('status' => 0, 'txt' => 'E-mail o password non corrispondenti'));
+            } else {
+                echo 'E-mail o password non corrispondenti';
+            }
         }
     }
-    
-    
-    public function reset_password_request() {
+
+
+    public function reset_password_request()
+    {
         $email = $this->input->post('email');
 
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            die(json_encode(array( 'status'=>0, 'txt'=> 'Inserisci un indirizzo e-mail valido.')));
+            die(json_encode(array('status' => 0, 'txt' => 'Inserisci un indirizzo e-mail valido.')));
         }
 
         $user = $this->db->get_where(LOGIN_ENTITY, [LOGIN_USERNAME_FIELD => $email])->row_array();
 
         if (empty($user)) {
-            die(json_encode(array( 'status'=>0, 'txt'=> 'Spiacenti, l\'indirizzo inserito non è registrato.' )));
+            die(json_encode(array('status' => 0, 'txt' => 'Spiacenti, l\'indirizzo inserito non è registrato.')));
         }
 
         $senderName = DEFAULT_EMAIL_SENDER;
@@ -131,33 +144,34 @@ class Access extends MY_Controller {
 
         $this->email->message(implode(PHP_EOL, $msg));
         $success = $this->email->send();
-        
-        
-        
+
+
+
         if (!$success) {
-            die(json_encode(array( 'status'=>0, 'txt'=> 'Si è verificato un errore inviando la mail. Riprovare più tardi o contattare l\'amministrazione.' )));
+            die(json_encode(array('status' => 0, 'txt' => 'Si è verificato un errore inviando la mail. Riprovare più tardi o contattare l\'amministrazione.')));
         }
 
         $checkHash = md5($email . self::SALT);
-        
-        echo json_encode(array( 'status'=>1, 'txt'=>base_url("access/recovery?sent=1&receiver={$email}&chk={$checkHash}") ));
+
+        echo json_encode(array('status' => 1, 'txt' => base_url("access/recovery?sent=1&receiver={$email}&chk={$checkHash}")));
         //redirect(base_url("access/recovery?sent=1&receiver={$email}&chk={$checkHash}"));
     }
 
-    public function reset_password($id, $hash) {
+    public function reset_password($id, $hash)
+    {
 
         if ($hash !== md5($id . self::SALT)) {
             show_error('Impossibile proseguire: codice di controllo non valido');
         }
-        
+
         $newPassword = generateRandomPassword();
-        $this->db->update(LOGIN_ENTITY, array(LOGIN_PASSWORD_FIELD => md5($newPassword)), array(LOGIN_ENTITY.'_id' => $id));
-        $user = $this->db->get_where(LOGIN_ENTITY, array(LOGIN_ENTITY.'_id' => $id))->row_array();
+        $this->db->update(LOGIN_ENTITY, array(LOGIN_PASSWORD_FIELD => md5($newPassword)), array(LOGIN_ENTITY . '_id' => $id));
+        $user = $this->db->get_where(LOGIN_ENTITY, array(LOGIN_ENTITY . '_id' => $id))->row_array();
         $email = $user[LOGIN_USERNAME_FIELD];
-        
+
         $senderName = DEFAULT_EMAIL_SENDER;
         $senderMail = DEFAULT_EMAIL_SYSTEM;
-        
+
         $this->load->library('email');
         $this->email->subject('Password cambiata')->to($email)->from($senderMail, $senderName);
         $msg = array(
@@ -175,13 +189,9 @@ class Access extends MY_Controller {
 
         redirect(base_url('access/password_resetted'));
     }
-        
-    public function password_resetted() {
+
+    public function password_resetted()
+    {
         $this->load->view('layout/password_resetted');
     }
-        
-        
-
 }
-
-?>
