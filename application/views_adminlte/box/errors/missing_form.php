d33e165bdbb1f66bc9f97cabf422f6c9105e1167
matteo <web@idra.h24hosting.com>
2019-10-28 11:40:11 +0100

    Manual commit

---
+++ application/views_adminlte/box/errors/missing_form.php
@@ -0,0 +1,15 @@
+<div class="error-page">
+    <h2 class="headline text-yellow" style="font-size:50px"> Oops!</h2>
+
+    <div class="error-content" style="margin-left:160px;">
+        <h3><i class="fa fa-warning text-yellow"></i>Form <?php echo $form_id; ?> not found.</h3>
+
+        <p>
+            We could not find the form you were looking for.<br />
+            Meanwhile, you may <a href="<?php echo base_url(); ?>">return to your dashboard</a><br />or contact administrators to solve this problem.
+        </p>
+
+
+    </div>
+    <!-- /.error-content -->
+</div>
