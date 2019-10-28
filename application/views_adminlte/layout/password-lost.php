5c48d87c841a0547bef50bd64878c1491e80f726
matteo <web@idra.h24hosting.com>
2019-10-28 11:21:49 +0100

    Commit automatico

--- application/views_adminlte/layout/password-lost.php
+++ application/views_adminlte/layout/password-lost.php
@@ -1,100 +1,103 @@
 <!DOCTYPE html>
 <!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
 <!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
-<!--[if !IE]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
-    <!-- BEGIN HEAD -->
-    <head>
-        <meta charset="utf-8" />
-        <title>Password dimenticata?</title>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-        <meta content="" name="description" />
-        <meta content="" name="author" />
-        <meta name="MobileOptimized" content="320">
+<!--[if !IE]><!-->
+<html lang="en" class="no-js">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
 
-        <!-- CORE LEVEL STYLES -->
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css?v={$this->config->item('version')}"); ?>"/>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/bower_components/font-awesome/css/font-awesome.min.css?v={$this->config->item('version')}"); ?>"/>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/bower_components/Ionicons/css/ionicons.min.css?v={$this->config->item('version')}"); ?>"/>
+<head>
+    <meta charset="utf-8" />
+    <title>Password dimenticata?</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+    <meta name="MobileOptimized" content="320">
 
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/dist/css/AdminLTE.min.css?v={$this->config->item('version')}"); ?>"/>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/plugins/iCheck/square/blue.css?v={$this->config->item('version')}"); ?>"/>
-        <!-- Google Font -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-    </head>
+    <!-- CORE LEVEL STYLES -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css?v={$this->config->item('version')}"); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/bower_components/font-awesome/css/font-awesome.min.css?v={$this->config->item('version')}"); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/bower_components/Ionicons/css/ionicons.min.css?v={$this->config->item('version')}"); ?>" />
 
-    <body class="hold-transition login-page">
-        <div class="login-box">
-            <div class="logo">
-                 <div class="text-center">
-                <?php if ($this->settings === array()): ?>
-                    <h2 class="login-logo">Your Company</h2>
-                <?php elseif ($this->settings['settings_company_logo']): ?>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/dist/css/AdminLTE.min.css?v={$this->config->item('version')}"); ?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/plugins/iCheck/square/blue.css?v={$this->config->item('version')}"); ?>" />
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="logo">
+            <div class="text-center">
+                <?php if ($this->settings === array()) : ?>
+                    <h2 class="login-logo"><?php e('La tua azienda'); ?></h2>
+                <?php elseif ($this->settings['settings_company_logo']) : ?>
                     <img src="<?php echo base_url_uploads("uploads/{$this->settings['settings_company_logo']}"); ?>" alt="logo" style="max-width: 360px;" />
-                <?php else: ?>
+                <?php else : ?>
                     <h2 class="text-danger"><?php echo $this->settings['settings_company_short_name']; ?></h2>
                 <?php endif; ?>
             </div>
-            </div>
-
-
-            <div class="login-box-body">
-                <?php if ($sent): ?>
-                    <h3><?php e("E-mail inviata correttamente");?></h3>
-                    <div class="alert alert-success">
-                        <p><?php e("Ti è stata inviata una mail all'indirizzo");?> <strong><?php echo $receiver; ?></strong><?php e("contenente il link necessario per effettuare il reset della password.");?> </p>
-                    </div>
-                <?php else: ?>
-                    <form id="lost" class="forget-form formAjax" action="<?php echo base_url('access/reset_password_request'); ?>" method="post" novalidate="novalidate" style="display: block;">
-                        <h4><?php e("Password dimenticata?"); ?></h4>
-                        <p><?php e("Inserisci il tuo indirizzo e-mail per resettare la password.");?></p>
-                        <div class="form-group has-feedback">
-
-                            <input  class="form-control" type="text" autocomplete="off" placeholder="Email" name="email">
-                            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-
-                        </div>
-                        <div class="form-group">
-                            <div class="controls">
-                                <div id="msg_lost" class="alert alert-danger hide"></div>
-                            </div>
-                        </div>
-
-                        <div class="form-actions">
-                            <div class="row">
-                                <div class="col-xs-6">
-                                    <a class="btn green" href="<?php echo base_url('access/login'); ?>">
-                                        <i class="m-icon-swapleft m-icon-white"></i>
-                                        Login
-                                    </a>
-                                </div>
-                                <div class="col-xs-6 text-right">
-                                    <button type="submit" class="btn btn-success btn-block btn-flat">
-                                        Submit
-                                    </button>            
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                <?php endif; ?>
-            </div>
-
         </div>
 
 
-        <div class="copyright"><?php /* powered by <a href="http://h2web.it" class="text-danger" target="_blank">H2 web</a> */ ?></div>
+        <div class="login-box-body">
+            <?php if ($sent) : ?>
+                <h3><?php e("E-mail inviata correttamente"); ?></h3>
+                <div class="alert alert-success">
+                    <p><?php e("Ti è stata inviata una mail all'indirizzo"); ?> <strong><?php echo $receiver; ?></strong><?php e("contenente il link necessario per effettuare il reset della password."); ?> </p>
+                </div>
+            <?php else : ?>
+                <form id="lost" class="forget-form formAjax" action="<?php echo base_url('access/reset_password_request'); ?>" method="post" novalidate="novalidate" style="display: block;">
+                    <h4><?php e("Password dimenticata?"); ?></h4>
+                    <p><?php e("Inserisci il tuo indirizzo e-mail per resettare la password."); ?></p>
+                    <div class="form-group has-feedback">
+
+                        <input class="form-control" type="text" autocomplete="off" placeholder="Email" name="email">
+                        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+
+                    </div>
+                    <div class="form-group">
+                        <div class="controls">
+                            <div id="msg_lost" class="alert alert-danger hide"></div>
+                        </div>
+                    </div>
+
+                    <div class="form-actions">
+                        <div class="row">
+                            <div class="col-xs-6">
+                                <a class="btn btn-primary" href="<?php echo base_url('access/login'); ?>">
+                                    <?php e('Accedi'); ?>
+                                </a>
+                            </div>
+                            <div class="col-xs-6 text-right">
+                                <button type="submit" class="btn btn-success">
+                                    <?php e('Azzera password'); ?>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            <?php endif; ?>
+        </div>
+
+    </div>
 
 
-        <!-- COMMON PLUGINS -->
-
-        <script  src="<?php echo base_url_template("template/adminlte/bower_components/jquery/dist/jquery.min.js?v={$this->config->item('version')}"); ?>"></script>
-        <script  src="<?php echo base_url_template("template/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js?v=" . $this->config->item('version')); ?>"></script>
-
-        <script  src="<?php echo base_url_template("template/adminlte/plugins/iCheck/icheck.min.js?v={$this->config->item('version')}"); ?>" ></script>
-        <!-- CUSTOM COMPONENTS -->
-        <script type="text/javascript" src="<?php echo base_url_scripts("script/js/submitajax.js?v={$this->config->item('version')}"); ?>"></script> 
+    <div class="copyright"><?php /* powered by <a href="http://h2web.it" class="text-danger" target="_blank">H2 web</a> */ ?></div>
 
 
+    <!-- COMMON PLUGINS -->
 
-    </body>
+    <script src="<?php echo base_url_template("template/adminlte/bower_components/jquery/dist/jquery.min.js?v={$this->config->item('version')}"); ?>"></script>
+    <script src="<?php echo base_url_template("template/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js?v=" . $this->config->item('version')); ?>"></script>
+
+    <script src="<?php echo base_url_template("template/adminlte/plugins/iCheck/icheck.min.js?v={$this->config->item('version')}"); ?>"></script>
+    <!-- CUSTOM COMPONENTS -->
+    <script type="text/javascript" src="<?php echo base_url_scripts("script/js/submitajax.js?v={$this->config->item('version')}"); ?>"></script>
+
+
+
+</body>
+
 </html>
--- application/views_adminlte/pages/api_manager.php
+++ application/views_adminlte/pages/api_manager.php
@@ -7,42 +7,42 @@
     <div class="col-lg-9" data-layout-box="58">
       <div class="box">
         <div class="box-header with-border">
-          <h3 class="box-title"><i class="fas fa-th"></i> Elenco Token</h3>
+          <h3 class="box-title"><i class="fas fa-th"></i> <?php e('Elenco Token'); ?></h3>
         </div>
-        
+
         <div class="box-body">
-          <table id='elenco_token' default-limit="10" class="table table-striped table-bordered table-hover js_datatable" >
+          <table id='elenco_token' default-limit="10" class="table table-striped table-bordered table-hover js_datatable">
             <thead>
               <tr>
-                <th >Data creazione</th>
-                <th >Utente</th>
-                <th >Token</th>
-                
-                <th >Ultimo utilizzo</th>
-                <th >Richieste</th>
-                <th >Errori</th>
-                <th >Attivo</th>
-                <th data-prevent-order>Action</th>
+                <th><?php e('Data creazione'); ?></th>
+                <th><?php e('Utente'); ?></th>
+                <th><?php e('Token'); ?></th>
+                <th><?php e('Ultimo utilizzo'); ?></th>
+                <th><?php e('Richieste'); ?></th>
+                <th><?php e('Errori'); ?></th>
+                <th><?php e('Attivo'); ?></th>
+                <th data-prevent-order><?php e('Azioni'); ?></th>
               </tr>
             </thead>
             <tbody>
               <?php foreach ($dati['tokens'] as $token) : ?>
-                <?php //debug($token, true); ?>
+                <?php //debug($token, true); 
+                  ?>
                 <tr class="odd gradeX" data-id="<?php echo $token['api_manager_tokens_id']; ?>">
                   <td><span class='hide'><?php echo $token['api_manager_tokens_creation_date']; ?></span><?php echo dateFormat($token['api_manager_tokens_creation_date']); ?></td>
                   <td><?php echo $token[LOGIN_NAME_FIELD]; ?> <?php echo $token[LOGIN_SURNAME_FIELD]; ?></td>
-                  
+
                   <td><?php echo $token['api_manager_tokens_token']; ?></td>
                   <td><span class='hide'><?php echo $token['api_manager_tokens_last_use_date']; ?></span><?php echo dateFormat($token['api_manager_tokens_last_use_date'], 'd/m/Y H:m:s'); ?></td>
                   <td><?php echo $token['api_manager_tokens_requests']; ?></td>
                   <td><?php echo $token['api_manager_tokens_errors']; ?></td>
-                  <td><?php echo ($token['api_manager_tokens_active']==DB_BOOL_TRUE)?'Sì':'No'; ?></td>
+                  <td><?php echo ($token['api_manager_tokens_active'] == DB_BOOL_TRUE) ? 'Sì' : 'No'; ?></td>
                   <td>
                     <div class="action-list">
                       <a href="<?php echo base_url("api_manager/permissions/{$token['api_manager_tokens_id']}"); ?>" class="btn btn-xs green js_open_modal">
                         <span class="fas fa-external-link-alt"></span>
                       </a>
-                      <a href="<?php echo base_url('api_manager/delete_token/'.$token['api_manager_tokens_id']); ?>" data-confirm-text="Sei sicuro di voler eliminare questo record" class="btn btn-danger btn-xs js_confirm_button js_link_ajax " data-toggle="tooltip" title="Elimina">
+                      <a href="<?php echo base_url('api_manager/delete_token/' . $token['api_manager_tokens_id']); ?>" data-confirm-text="Sei sicuro di voler eliminare questo record" class="btn btn-danger btn-xs js_confirm_button js_link_ajax " data-toggle="tooltip" title="Elimina">
                         <span class="fas fa-times"></span>
                       </a>
                     </div>
@@ -54,73 +54,73 @@
         </div>
       </div>
     </div>
-    
+
     <div class="col-lg-3">
       <div class="box">
         <div class="box-header with-border">
-          <h3 class="box-title"><i class=" fas fa-edit"></i> Autorizza nuovo token</h3>
+          <h3 class="box-title"><i class=" fas fa-edit"></i> <?php e('Autorizza nuovo token'); ?></h3>
         </div>
-        
+
         <div class="box-body">
           <form id='form_token' role="form" method="post" action="<?php echo base_url("api_manager/add_token"); ?>" class="formAjax" enctype="multipart/form-data">
             <div class="form-body">
-              
+
               <div class="row">
                 <div class="col-lg-12">
-                  <div class="form-group" >
-                    <label class="control-label">Utente</label>
-                    <select class="form-control _select2_standard  field_101" name="api_manager_tokens_user" data-source-field="" data-ref="api_manager_tokens_user" data-val="" >
+                  <div class="form-group">
+                    <label class="control-label"><?php e('Utente'); ?></label>
+                    <select class="form-control _select2_standard  field_101" name="api_manager_tokens_user" data-source-field="" data-ref="api_manager_tokens_user" data-val="">
                       <?php foreach ($dati['users'] as $id => $name) : ?>
-                        <option value="<?php echo $id; ?>" ><?php echo $name; ?></option>
+                        <option value="<?php echo $id; ?>"><?php echo $name; ?></option>
                       <?php endforeach; ?>
                     </select>
                   </div>
                 </div>
               </div>
               <div class="row">
-                
+
                 <div class="col-lg-6">
-                  <div class="form-group" >
-                    <label class="control-label">Token pubblico</label>
+                  <div class="form-group">
+                    <label class="control-label"><?php e('Token pubblico'); ?></label>
                     <input type="text" name="api_manager_tokens_token" placeholder="Autogenerato..." class="form-control" placeholder="1000" value="">
                   </div>
                 </div>
               </div>
               <div class="row">
                 <div class="col-lg-6">
-                  <div class="form-group" >
-                    <label class="control-label">ms tra una richiesta e un'altra</label>
+                  <div class="form-group">
+                    <label class="control-label"><?php e('ms tra una richiesta e l\'altra'); ?></label>
                     <input type="text" name="api_manager_tokens_ms_between_requests" class="form-control" placeholder="1000" value="">
                   </div>
                 </div>
                 <div class="col-lg-6">
-                  <div class="form-group" >
-                    <label class="control-label">Richieste al minuto (max)</label>
+                  <div class="form-group">
+                    <label class="control-label"><?php e('Richieste al minuto (max)'); ?></label>
                     <input type="text" name="api_manager_tokens_limit_per_minute" class="form-control" placeholder="50" value="">
                   </div>
                 </div>
               </div>
-              
+
               <div class="row">
                 <div class="col-lg-6">
-                  <div class="form-group" >
-                    <label class="control-label">Attivo</label>
+                  <div class="form-group">
+                    <label class="control-label"><?php e('Attivo'); ?></label>
                     <input name="api_manager_tokens_active" type="checkbox" class="_form-control" value="<?php echo DB_BOOL_TRUE; ?>">
                   </div>
                 </div>
-                
+
               </div>
-              
+
               <div class="row">
                 <div class="col-md-12">
                   <div id='msg_form_token' class="alert alert-danger hide"></div>
                 </div>
               </div>
             </div>
-            
+
             <div class="form-actions right">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Annulla</button>
-              <button type="submit" class="btn btn-success">Salva</button>
+              <button type="button" class="btn btn-default" data-dismiss="modal"><?php e('Annulla'); ?></button>
+              <button type="submit" class="btn btn-success"><?php e('Salva'); ?></button>
             </div>
           </form>
         </div>
@@ -130,7 +130,7 @@
 </section>
 
 <script>
-$(document).ready(function() {
-  $('body').addClass('page-sidebar-closed').find('.page-sidebar-menu').addClass('page-sidebar-menu-closed');
-});
+  $(document).ready(function() {
+    $('body').addClass('page-sidebar-closed').find('.page-sidebar-menu').addClass('page-sidebar-menu-closed');
+  });
 </script>
