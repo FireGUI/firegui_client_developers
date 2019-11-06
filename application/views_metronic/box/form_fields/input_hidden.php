f2fa9647705e316bcbfc457ebbc5a35bffabd68c
matteo <web@idra.h24hosting.com>
2019-11-06 06:49:58 +0100

    Removed metronic template and view folder

--- application/views_metronic/box/form_fields/input_hidden.php
+++
@@ -1,1 +0,0 @@
-<input type="hidden" name="<?php echo $field['fields_name']; ?>" class="<?php echo $class ?>" value="<?php echo $value; ?>" />
--- application/views_metronic/pages/layout_unaccessible.php
+++
@@ -1,1 +0,0 @@
-<h3 class="page-title">Spiacenti! La pagina da te cercata non esiste, oppure non hai i permessi per accedervi.</h3>
