df760b90bd14e2b151aa68da77157af460f8b98b
Matteo <>
2019-12-17 15:08:51 +0100

    Added in .htaccess FIREGUI_CLIENT_TEMPLATE enviroment variable Removed hardcoded template Bugfix for undefined $_SERVER vars when run in cli Added function file_put_contents_and_create_dir Fixed file_put_contents in getCacheAdapter when folder does not exist Bug fix in getRememberedUser when cookie does not exists or is invalid Config.php core file well-formatted

--- application/config/enviroment.php
+++ application/config/enviroment.php
@@ -1,6 +1,8 @@
 <?php
+if (!defined('THEME_CSS_PATH')) { //Avoid double include
 
-/*
+
+    /*
  * ============================================================
  * Base configurations
  * ============================================================
@@ -9,14 +11,14 @@
  * CUSTOM_CSS_PATH     Path to custom css
  * UPLOAD_DEPTH_LEVEL   Annidated folders to create for uploads directory
  */
-define('THEME_CSS_PATH', null);
-define('CUSTOM_CSS_PATH', null);
-define('UPLOAD_DEPTH_LEVEL', 3);
+    define('THEME_CSS_PATH', null);
+    define('CUSTOM_CSS_PATH', null);
+    define('UPLOAD_DEPTH_LEVEL', 3);
 
-define('CUSTOM_FAVICON', null);
+    define('CUSTOM_FAVICON', null);
 
 
-/*
+    /*
  * ============================================================
  * E-mail system
  * ============================================================
@@ -25,12 +27,101 @@ define('CUSTOM_FAVICON', null);
  * DEFAULT_EMAIL_FROM       "From" default header
  * DEFAULT_EMAIL_REPLY      "Reply To" default header
  */
-define('DEFAULT_EMAIL_SYSTEM', 'no-reply@yourdomain.com');
-define('DEFAULT_EMAIL_SENDER', 'FireGUI');
-define('DEFAULT_EMAIL_FROM', 'From: No Reply <no-reply@yourdomain.com>');
-define('DEFAULT_EMAIL_REPLY', 'Reply-To: No Reply <no-reply@yourdomain.com>');
+    define('DEFAULT_EMAIL_SYSTEM', 'no-reply@yourdomain.com');
+    define('DEFAULT_EMAIL_SENDER', 'FireGUI');
+    define('DEFAULT_EMAIL_FROM', 'From: No Reply <no-reply@yourdomain.com>');
+    define('DEFAULT_EMAIL_REPLY', 'Reply-To: No Reply <no-reply@yourdomain.com>');
 
-/*
+    /*
+ 
+
+    /*
+ * ============================================================
+ * Base entities
+ * ============================================================
+ * LOGIN_ENTITY             Entity to use for the login
+ * LOGIN_USERNAME_FIELD     Username/email field for login
+ * LOGIN_PASSWORD_FIELD     Password field for login
+ * LOGIN_ACTIVE_FIELD       Active field for login
+ * LOGIN_NAME_FIELD         Name field for login
+ * LOGIN_SURNAME_FIELD      Surname field for login
+ * LOGIN_IMG_FIELD          Avatar field for login
+ *
+ * LANG_ENTITY              Language entity
+ * LANG_CODE_FIELD          Language code field
+ * LANG_NAME_FIELD          Language name/identifier field
+ * LANG_DEFAULT_FIELD       Language "is default" field
+ */
+    define('LOGIN_ENTITY',          'users');
+    define('LOGIN_USERNAME_FIELD',  'users_email');
+    define('LOGIN_PASSWORD_FIELD',  'users_password');
+    define('LOGIN_ACTIVE_FIELD',    'users_active');
+    define('LOGIN_NAME_FIELD',      'users_first_name');
+    define('LOGIN_SURNAME_FIELD',   'users_last_name');
+    define('LOGIN_IMG_FIELD',       'users_avatar');
+
+    define('LANG_ENTITY',       'languages');
+    define('LANG_CODE_FIELD',   'languages_code');
+    define('LANG_NAME_FIELD',   'languages_name');
+    //define('LANG_DEFAULT_FIELD', 'languages_default');
+
+    define('MIN_SEARCH_CHARS', 3);
+    //define('STRICT_SEARCH', false);
+    define('EXPLODE_SPACES', true);
+    define('DEFAULT_GRID_LIMIT', 10);
+
+    define('API_MANAGER_PRIVATE_KEY', '*******');
+    define('API_MANAGER_CRM_PASSPARTOUT', '*******');
+
+    //MD5 Passepartout for login password (example: if you want to login with every user email using the string 'your-secret-password', you should copy&paste the md5 of 'your-secret-password' here.)
+    define('PASSEPARTOUT', '*********************************');
+
+    /* ============
+ * Custom colors
+ * ============ */
+    define('TOPBAR_COLOR', '#b22222');
+    define('TOPBAR_HOVER', '#851717');
+    define('LOGIN_COLOR', '#b22222');
+    define('LOGIN_TITLE_COLOR', '#FFFFFF');
+
+    /*
+ * ============================================================
+ * Base URL ADMIN 
+ * ============================================================
+ *
+ */
+}
+if (!function_exists('base_url_admin')) {
+    function base_url_admin($uri = '')
+    {
+        if (function_exists('base_url')) {
+            return base_url($uri);
+        } else {
+            return $uri;
+        }
+    }
+
+    /*
+ * ============================================================
+ * Base URL Template , Base URL Script, Base URL Uploads
+ * ============================================================
+ * 
+ */
+    function base_url_template($uri = '')
+    {
+        return base_url_admin($uri);
+    }
+    function base_url_scripts($uri = '')
+    {
+        return base_url_admin($uri);
+    }
+    function base_url_uploads($uri = '')
+    {
+        return base_url_admin($uri);
+    }
+}
+
+/* 
  * ============================================================
  * Additional and customizable configurations
  * ============================================================
@@ -49,86 +140,3 @@ $config = array(
         'Content-Transfer-Encoding' => '8bit',
     )
 );
-
-/*
- * ============================================================
- * Base entities
- * ============================================================
- * LOGIN_ENTITY             Entity to use for the login
- * LOGIN_USERNAME_FIELD     Username/email field for login
- * LOGIN_PASSWORD_FIELD     Password field for login
- * LOGIN_ACTIVE_FIELD       Active field for login
- * LOGIN_NAME_FIELD         Name field for login
- * LOGIN_SURNAME_FIELD      Surname field for login
- * LOGIN_IMG_FIELD          Avatar field for login
- *
- * LANG_ENTITY              Language entity
- * LANG_CODE_FIELD          Language code field
- * LANG_NAME_FIELD          Language name/identifier field
- * LANG_DEFAULT_FIELD       Language "is default" field
- */
-define('LOGIN_ENTITY',          'users');
-define('LOGIN_USERNAME_FIELD',  'users_email');
-define('LOGIN_PASSWORD_FIELD',  'users_password');
-define('LOGIN_ACTIVE_FIELD',    'users_active');
-define('LOGIN_NAME_FIELD',      'users_first_name');
-define('LOGIN_SURNAME_FIELD',   'users_last_name');
-define('LOGIN_IMG_FIELD',       'users_avatar');
-
-define('LANG_ENTITY',       'languages');
-define('LANG_CODE_FIELD',   'languages_code');
-define('LANG_NAME_FIELD',   'languages_name');
-//define('LANG_DEFAULT_FIELD', 'languages_default');
-
-define('MIN_SEARCH_CHARS', 3);
-//define('STRICT_SEARCH', false);
-define('EXPLODE_SPACES', true);
-define('DEFAULT_GRID_LIMIT', 10);
-
-define('API_MANAGER_PRIVATE_KEY', '*******');
-define('API_MANAGER_CRM_PASSPARTOUT', '*******');
-
-//MD5 Passepartout for login password (example: if you want to login with every user email using the string 'your-secret-password', you should copy&paste the md5 of 'your-secret-password' here.)
-define('PASSEPARTOUT', '*********************************');
-
-/* ============
- * Custom colors
- * ============ */
-define('TOPBAR_COLOR', '#b22222');
-define('TOPBAR_HOVER', '#851717');
-define('LOGIN_COLOR', '#b22222');
-define('LOGIN_TITLE_COLOR', '#FFFFFF');
-
-/*
- * ============================================================
- * Base URL ADMIN 
- * ============================================================
- *
- */
-function base_url_admin($uri = '')
-{
-    if (function_exists('base_url')) {
-        return base_url($uri);
-    } else {
-        return $uri;
-    }
-}
-
-/*
- * ============================================================
- * Base URL Template , Base URL Script, Base URL Uploads
- * ============================================================
- * 
- */
-function base_url_template($uri = '')
-{
-    return base_url_admin($uri);
-}
-function base_url_scripts($uri = '')
-{
-    return base_url_admin($uri);
-}
-function base_url_uploads($uri = '')
-{
-    return base_url_admin($uri);
-}
