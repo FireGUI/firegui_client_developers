a446bd20c6a623d60a070eabb06182c7facef9f3
Michael <michael@h2web.it>
2019-12-20 09:41:00 +0100

    Aggiunta classe "nowrap" sulle grid

--- application/views_adminlte/pages/layouts/grids/default.php
+++ application/views_adminlte/pages/layouts/grids/default.php
@@ -1,18 +1,18 @@
-<?php if (empty($grid_data['data'])): ?>
+<?php if (empty($grid_data['data'])) : ?>
     <p>Nessun dato disponibile</p>
-<?php else: ?>
-    <table class="table table-condensed table-striped table-bordered table-hover <?php echo $grid['grids']['grids_append_class']; ?>">
+<?php else : ?>
+    <table class="table table-condensed table-striped nowrap table-bordered table-hover <?php echo $grid['grids']['grids_append_class']; ?>">
         <thead>
             <tr>
-                <?php foreach ($grid['grids_fields'] as $field): ?>
+                <?php foreach ($grid['grids_fields'] as $field) : ?>
                     <th><?php echo $field['grids_fields_column_name'];  ?></th>
                 <?php endforeach; ?>
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($grid_data['data'] as $dato): ?>
+            <?php foreach ($grid_data['data'] as $dato) : ?>
                 <tr class="odd gradeX" data-id="<?php echo $dato[$grid['grids']['entity_name'] . "_id"]; ?>">
-                    <?php foreach ($grid['grids_fields'] as $field): ?>
+                    <?php foreach ($grid['grids_fields'] as $field) : ?>
                         <td><?php echo $this->datab->build_grid_cell($field, $dato); ?></td>
                     <?php endforeach; ?>
                 </tr>
