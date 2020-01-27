fd400d73986ec7ef494021f2f6e4d47185cf0ce8
Manuel Aiello <manuel@h2web.it>
2020-01-27 17:40:17 +0100

    Changes configs

--- application/config/config.php
+++ application/config/config.php
@@ -92,7 +92,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']    = 'italiano';
+$config['language']    = 'english';
 
 /*
 |--------------------------------------------------------------------------
