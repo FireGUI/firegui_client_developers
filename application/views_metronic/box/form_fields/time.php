57f8e1377fdb20227b9412cb3ae39a6307214f21
matteo <web@idra.h24hosting.com>
2020-02-04 15:13:09 +0100

    Commit new release

---
+++ application/views_metronic/box/form_fields/time.php
@@ -0,0 +1,22 @@
+<?php
+if ($value) {
+    $timeChunks = explode(':', $value);
+    if (count($timeChunks) !== 2 OR ! is_numeric($timeChunks[0]) OR ! is_numeric($timeChunks[1])) {
+        $value = null;
+    }
+}
+?>
+<?php echo $label; ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="input-group <?php echo $class ?>" <?php echo $onclick; ?>>
+            <input name="<?php echo $field['fields_name']; ?>" type="text" class="form-control js_form_timepicker" value="<?php echo $value; ?>" data-default-time="<?php echo $value; ?>" />
+            <span class="input-group-btn">
+                <button class="btn default" type="button"><i class="far fa-clock"></i></button>
+            </span>
+        </div>
+<?php echo $help; ?>
+    </div>
+</div>
+<div class="clearfix"></div>
+
---
+++ application/views_metronic/pages/layouts/grids/datatable_ajax_inline.php
@@ -0,0 +1,17 @@
+<a class="js_datatable_inline_add btn btn-success btn-xs pull-right" data-grid-id="<?php echo $grid['grids']['grids_id']; ?>">Nuovo valore</a>
+<div class="clearfix"></div>
+<br/>
+
+<table <?php echo "id='grid_{$grid['grids']['grids_id']}'" ?> default-limit="<?php echo (defined('DEFAULT_GRID_LIMIT'))?DEFAULT_GRID_LIMIT:10; ?>" class="table table-striped table-bordered table-hover table-middle js_ajax_datatable js_datatable_inline <?php echo $grid['grids']['grids_append_class']; ?>" data-value-id="<?php echo $value_id; ?>" data-entity="<?php echo $grid['grids']['entity_name']; ?>" <?php // if($grid['grids']['grids_order_by']) echo 'data-prevent-order' ?> data-grid-id="<?php echo $grid['grids']['grids_id']; ?>">
+    <thead>
+        <tr>
+            <?php foreach ($grid['grids_fields'] as $field): ?>
+                <th data-name="<?php echo $field['fields_name']; ?>"><?php echo $field['grids_fields_column_name']; ?></th>
+            <?php endforeach; ?>
+
+            <th data-prevent-order>&nbsp;</th>
+            <th data-prevent-order>&nbsp;</th>
+        </tr>
+    </thead>
+    <tbody></tbody>
+</table>
