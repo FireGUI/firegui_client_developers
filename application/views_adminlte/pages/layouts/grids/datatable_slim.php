c288ec51892fac5088c55e704854da9870d62e06
Matteo <>
2020-02-04 09:55:42 +0100

    Fix csrf on datatable slim and with subentity

--- application/views_adminlte/pages/layouts/grids/datatable_slim.php
+++ application/views_adminlte/pages/layouts/grids/datatable_slim.php
@@ -2,8 +2,8 @@
     <p>Nessun dato disponibile</p>
 <?php else : ?>
     <div class="table-scrollable table-scrollable-borderless">
-        <table <?php echo "id='grid_{$grid['grids']['grids_id']}'"; ?> default-limit="<?php echo (defined('DEFAULT_GRID_LIMIT')) ? DEFAULT_GRID_LIMIT : 10; ?>" class="table table-striped table-bordered nowrap table-condensed table-hover js_datatable_slim <?php echo $grid['grids']['grids_append_class']; ?>" <?php // if ($grid['grids']['grids_order_by']) echo 'data-prevent-order' 
-                                                                                                                                                                                                                                                                                                                    ?>>
+        <table <?php echo "id='grid_{$grid['grids']['grids_id']}'"; ?> data-csrf="<?php echo base64_encode(json_encode(get_csrf())); ?>" default-limit="<?php echo (defined('DEFAULT_GRID_LIMIT')) ? DEFAULT_GRID_LIMIT : 10; ?>" class="table table-striped table-bordered nowrap table-condensed table-hover js_datatable_slim <?php echo $grid['grids']['grids_append_class']; ?>" <?php // if ($grid['grids']['grids_order_by']) echo 'data-prevent-order' 
+                                                                                                                                                                                                                                                                                                                                                                                        ?>>
             <thead>
                 <tr>
                     <?php foreach ($grid['grids_fields'] as $field) : ?>
