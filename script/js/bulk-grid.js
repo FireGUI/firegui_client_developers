8f72169ca1e91c3335fce578676e5749b39d3384
Michael <michael@h2web.it>
2020-03-10 14:27:28 +0100

    Fix csrf bulkable

--- script/js/bulk-grid.js
+++ script/js/bulk-grid.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
+    var csrf_token = JSON.parse(atob($('body').data('csrf')));
+    var csrf_token_name = csrf_token.name;
+    var csrf_token_hash = csrf_token.hash;
+
     $('.js-bulk-action > option[value=""]').attr('selected', 'selected').trigger('change');
     $("input:checkbox.js_bulk_check,.js-bulk-select-all").removeAttr('checked').trigger('change');
     $.uniform.update();
     $('.js-bulk-select-all').on('click', function () {
-        
+
         var grid_container = $(this).closest('table.table').closest('.grid,.tab-pane');
-        
+
         if ($(this).is(':checked')) {
             $('input[type="checkbox"].js_bulk_check', grid_container).attr('checked', 'checked').trigger('change');
         } else {
@@ -13,61 +17,61 @@ $(document).ready(function () {
         }
         //Questo ridisegna le checkbox metronic
         $.uniform.update();
-        
-        var chkbx_ids = $("input:checkbox.js_bulk_check:checked", grid_container).map(function(){
+
+        var chkbx_ids = $("input:checkbox.js_bulk_check:checked", grid_container).map(function () {
             return $(this).val();
         }).get();
-        
+
         if (chkbx_ids.length == 0) {
             $('.js-bulk-first-option', grid_container).html('');
             $('.js-bulk-action option', grid_container).attr('disabled', 'disabled');
         } else {
-            $('.js-bulk-first-option', grid_container).html(chkbx_ids.length+' selected');
+            $('.js-bulk-first-option', grid_container).html(chkbx_ids.length + ' selected');
             $('.js-bulk-action option', grid_container).removeAttr('disabled');
         }
-        
+
     });
-    
-    $("table").on('click',"input:checkbox.js_bulk_check",  function () {
+
+    $("table").on('click', "input:checkbox.js_bulk_check", function () {
         //Questo ridisegna le checkbox metronic
         $.uniform.update();
         var grid_container = $(this).closest('table.table').closest('.grid,.tab-pane');
-        var chkbx_ids = $("input:checkbox.js_bulk_check:checked", grid_container).map(function(){
+        var chkbx_ids = $("input:checkbox.js_bulk_check:checked", grid_container).map(function () {
             return $(this).val();
         }).get();
         //alert(chkbx_ids.length);
         if (chkbx_ids.length == 0) {
-            
-            $('.js-bulk-first-option',grid_container).html('');
+
+            $('.js-bulk-first-option', grid_container).html('');
             $('.js-bulk-action option', grid_container).attr('disabled', 'disabled');
         } else {
             console.log("UAI?");
-            $('.js-bulk-first-option',grid_container).html(chkbx_ids.length+' selected');
+            $('.js-bulk-first-option', grid_container).html(chkbx_ids.length + ' selected');
             $('.js-bulk-action option', grid_container).removeAttr('disabled');
         }
     });
-    
+
     $('.js-bulk-action').on('change', function () {
         var grid_container = $(this).closest('div[data-layout-box]');
         if ($(this).val() != '') {
-            var chkbx_ids = $("input:checkbox.js_bulk_check:checked", grid_container).map(function(){
+            var chkbx_ids = $("input:checkbox.js_bulk_check:checked", grid_container).map(function () {
                 return $(this).val();
-              }).get();
-            
+            }).get();
+
             if ($(this).val() == 'bulk_edit') {
-                
+
                 var form_id = $(this).find(":selected").data('form_id');
-                loadModal(base_url+'get_ajax/modal_form/'+form_id, {"ids":chkbx_ids}, null, 'POST');
+                loadModal(base_url + 'get_ajax/modal_form/' + form_id, { [token_name]: token_hash, "ids": chkbx_ids }, null, 'POST');
             } else if ($(this).val() == 'bulk_delete') {
-                var r = confirm("Confermi di voler eliminare "+chkbx_ids.length+" righe?");
+                var r = confirm("Confermi di voler eliminare " + chkbx_ids.length + " righe?");
                 if (r == true) {
-                    var url = base_url+'db_ajax/generic_delete/'+$(this).data('entity-name');
-                    var data = {"ids":chkbx_ids};
+                    var url = base_url + 'db_ajax/generic_delete/' + $(this).data('entity-name');
+                    var data = { [token_name]: token_hash, "ids": chkbx_ids };
                     $.ajax({
                         url: url,
                         type: 'POST',
                         data: data,
-                        success: function(json) {
+                        success: function (json) {
                             location.reload();
                         }
                     });
