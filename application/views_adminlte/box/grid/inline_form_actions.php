704ae538f20c14e816022bd6860e0b5011cbd4ab
Matteo <>
2020-03-20 10:55:31 +0100

    Added modal extra large for fullscreen modals

--- application/views_adminlte/box/grid/inline_form_actions.php
+++ application/views_adminlte/box/grid/inline_form_actions.php
@@ -56,6 +56,8 @@
                             $url = "{base_url}get_ajax/layout_modal/{$custom_action['grids_actions_layout']}/$id";
                         } elseif ('modal_large' == $custom_action['grids_actions_mode']) {
                             $url = "{base_url}get_ajax/layout_modal/{$custom_action['grids_actions_layout']}/$id?_size=large";
+                        } elseif ('modal_extra' == $custom_action['grids_actions_mode']) {
+                            $url = "{base_url}get_ajax/layout_modal/{$custom_action['grids_actions_layout']}/$id?_size=extra";
                         }
                     } elseif (!empty($custom_action['grids_actions_type']) && 'delete' == $custom_action['grids_actions_type']) {
                         $confirm = true;
@@ -66,6 +68,8 @@
                             $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id";
                         } elseif ('modal_large' == $custom_action['grids_actions_mode']) {
                             $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id?_size=large";
+                        } elseif ('modal_extra' == $custom_action['grids_actions_mode']) {
+                            $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id?_size=extra";
                         }
                     } else {
                         die('TODO type!!!');
@@ -82,7 +86,7 @@
 
                 ?>
                 <span <?php echo $custom_action['grids_actions_name'] ? "data-toggle='tooltip' title='{$custom_action['grids_actions_name']}'" : null; ?>>
-                    <a class="js-action_button btn btn-xs btn-primary<?php if ($confirm) : ?> js_confirm_button js_link_ajax<?php endif; ?> <?php if (in_array($custom_action['grids_actions_mode'], ['modal', 'modal_large'])) echo 'js_open_modal'; ?>" href="<?php echo $url; ?>" <?php if ($custom_action['grids_actions_mode'] == 'new_tab') : ?>target="_blank" <?php endif; ?>style="background-color: <?php echo ($custom_action['grids_actions_color']) ?: '#CCCCCC'; ?>" <?php if ($confirm) : ?> data-confirm-text="<?php e('Sei sicuro di voler eliminare questo record?'); ?>" data-toggle="tooltip" <?php endif; ?>>
+                    <a class="js-action_button btn btn-xs btn-primary<?php if ($confirm) : ?> js_confirm_button js_link_ajax<?php endif; ?> <?php if (in_array($custom_action['grids_actions_mode'], ['modal', 'modal_large', 'modal_extra'])) echo 'js_open_modal'; ?>" href="<?php echo $url; ?>" <?php if ($custom_action['grids_actions_mode'] == 'new_tab') : ?>target="_blank" <?php endif; ?>style="background-color: <?php echo ($custom_action['grids_actions_color']) ?: '#CCCCCC'; ?>" <?php if ($confirm) : ?> data-confirm-text="<?php e('Sei sicuro di voler eliminare questo record?'); ?>" data-toggle="tooltip" <?php endif; ?>>
                         <span class="<?php echo $custom_action['grids_actions_icon']; ?>" style="color:white !important;"></span>
                     </a>
                 </span>
