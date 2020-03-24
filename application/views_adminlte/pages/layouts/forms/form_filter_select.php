62978351ea2e4f9c835447fd3ef28c3bdfdb2fdb
Michael <michael@h2web.it>
2020-03-24 15:06:57 +0100

    Fix trads

--- application/views_adminlte/pages/layouts/forms/form_filter_select.php
+++ application/views_adminlte/pages/layouts/forms/form_filter_select.php
@@ -11,23 +11,23 @@ $where_data = array_combine(array_key_map($_sess_where_data, 'field_id'), $_sess
         <div class="row">
             <?php foreach ($form['forms_fields'] as $k => $field) : ?>
                 <?php //debug($field); 
-                    ?>
+                ?>
                 <div class="<?php echo sprintf('col-lg-%d', $field['size'] ?: 6); ?>">
                     <?php
-                        //debug($where_data,true);
-                        $value = empty($where_data[$field['id']]['value']) ? NULL : $where_data[$field['id']]['value'];
-                        if ($field['datatype'] == 'INT4RANGE') {
-                            $oper  = empty($where_data[$field['id']]['operator']) ? 'rangein' : $where_data[$field['id']]['operator'];
-                        } else {
-                            $oper  = empty($where_data[$field['id']]['operator']) ? 'eq' : $where_data[$field['id']]['operator'];
-                        }
+                    //debug($where_data,true);
+                    $value = empty($where_data[$field['id']]['value']) ? NULL : $where_data[$field['id']]['value'];
+                    if ($field['datatype'] == 'INT4RANGE') {
+                        $oper  = empty($where_data[$field['id']]['operator']) ? 'rangein' : $where_data[$field['id']]['operator'];
+                    } else {
+                        $oper  = empty($where_data[$field['id']]['operator']) ? 'eq' : $where_data[$field['id']]['operator'];
+                    }
 
-                        if (is_array($value)) {
-                            // Se per caso ho forzato questo valore ad essere array,
-                            // intanto lo implodo, poi vedrò come gestirlo
-                            $value = implode(',', $value);
-                        }
-                        ?>
+                    if (is_array($value)) {
+                        // Se per caso ho forzato questo valore ad essere array,
+                        // intanto lo implodo, poi vedrò come gestirlo
+                        $value = implode(',', $value);
+                    }
+                    ?>
 
                     <div class="form-group">
                         <input type="hidden" class="js-filter-field" name="conditions[<?php echo $k; ?>][field_id]" value="<?php echo $field['id']; ?>" />
@@ -42,49 +42,49 @@ $where_data = array_combine(array_key_map($_sess_where_data, 'field_id'), $_sess
                                 </span>
                             </div>
                         <?php elseif ($field['datatype'] == DB_BOOL_IDENTIFIER) : ?>
-                            <button type="button" class="btn-link" onclick="$('.field_<?php echo $field['id']; ?>', $('#<?php echo "form_{$form['forms']['forms_id']}" ?>')).attr('checked', false)" data-toggle="tooltip" title="Rimuovi selezione">
+                            <button type="button" class="btn-link" onclick="$('.field_<?php echo $field['id']; ?>', $('#<?php echo "form_{$form['forms']['forms_id']}" ?>')).attr('checked', false)" data-toggle="tooltip" title="<?php e('Remove selection'); ?>">
                                 <small><i class="fas fa-times"></i></small>
                             </button>
                             <div class="col-xs-12">
                                 <label class="radio-inline">
                                     <input type="radio" name="conditions[<?php echo $k; ?>][value]" class="toggle field_<?php echo $field['id']; ?>" value="<?php echo DB_BOOL_TRUE; ?>" <?php echo (($value == DB_BOOL_TRUE) ? 'checked' : ''); ?> />
-                                    Si
+                                    <?php e('Yes'); ?>
                                 </label>
                                 <label class="radio-inline">
                                     <input type="radio" name="conditions[<?php echo $k; ?>][value]" class="toggle field_<?php echo $field['id']; ?>" value="<?php echo DB_BOOL_FALSE; ?>" <?php echo (($value == DB_BOOL_FALSE) ? 'checked' : ''); ?> />
-                                    No
+                                    <?php e('No'); ?>
                                 </label>
                             </div>
                         <?php elseif ($field['datatype'] == 'INT4RANGE') : ?>
                             <?php
 
-                                    //debug($field,true);
+                            //debug($field,true);
 
-                                    //Mi costruisco i parametri basati sui min/max dell'entità.
-                                    $field_completo = $this->datab->get_field($field['id']);
-                                    $entity = $this->datab->get_entity($field_completo['fields_entity_id']);
-                                    //var_dump($field['min']);
-                                    if ($field['min'] === '') {
-                                        $min = $this->db->query("SELECT MIN(LOWER({$field_completo['fields_name']})) as min FROM {$entity['entity_name']}");
-                                        $min = ($min->num_rows() == 1) ? $min->row()->min : 0;
-                                    } else {
-                                        $min = $field['min'];
-                                    }
-                                    if ($field['max'] === '') {
-                                        $max = $this->db->query("SELECT MAX(UPPER({$field_completo['fields_name']})) as max FROM {$entity['entity_name']}");
-                                        $max = ($max->num_rows() == 1) ? $max->row()->max : 1;
-                                    } else {
-                                        $max = $field['max'];
-                                    }
-                                    $step = (int) ($max - $min) / 20;
-                                    if ($value) {
-                                        $value_expl = explode(',', $value);
-                                    } else {
-                                        $value_expl = [$min, $max];
-                                    }
+                            //Mi costruisco i parametri basati sui min/max dell'entità.
+                            $field_completo = $this->datab->get_field($field['id']);
+                            $entity = $this->datab->get_entity($field_completo['fields_entity_id']);
+                            //var_dump($field['min']);
+                            if ($field['min'] === '') {
+                                $min = $this->db->query("SELECT MIN(LOWER({$field_completo['fields_name']})) as min FROM {$entity['entity_name']}");
+                                $min = ($min->num_rows() == 1) ? $min->row()->min : 0;
+                            } else {
+                                $min = $field['min'];
+                            }
+                            if ($field['max'] === '') {
+                                $max = $this->db->query("SELECT MAX(UPPER({$field_completo['fields_name']})) as max FROM {$entity['entity_name']}");
+                                $max = ($max->num_rows() == 1) ? $max->row()->max : 1;
+                            } else {
+                                $max = $field['max'];
+                            }
+                            $step = (int) ($max - $min) / 20;
+                            if ($value) {
+                                $value_expl = explode(',', $value);
+                            } else {
+                                $value_expl = [$min, $max];
+                            }
 
-                                    //debug($value,true);
-                                    ?>
+                            //debug($value,true);
+                            ?>
                             <input id="range_field_<?php echo $field['id']; ?>" type="text" value="<?php echo $value; ?>" />
                             <input type="hidden" class="js-filter-field" id="range_field_value_<?php echo $field['id']; ?>" name="conditions[<?php echo $k; ?>][value]" value="<?php echo $value; ?>" />
                             <script>
@@ -115,21 +115,21 @@ $where_data = array_combine(array_key_map($_sess_where_data, 'field_id'), $_sess
                             <?php if ($field['filterref']) : ?>
                                 <!--<input type="hidden" name="conditions[<?php echo $k; ?>][value]" data-ref="<?php echo $field['filterref']; ?>" data-referer="<?php echo $field['name']; ?>" class="form-control  __js_select_ajax field_<?php echo $field['id']; ?>" value="<?php echo $value; ?>" />-->
                                 <?php
-                                            $data = $this->apilib->runDataProcessing($field['filterref'], 'pre-search');
-                                            $where = '1';
-                                            foreach ($data as $campo => $valore) {
-                                                if ($campo) {
-                                                    $where .= " AND {$campo} = '$valore'";
-                                                } else {
-                                                    $where .= " AND $valore";
-                                                }
-                                            }
+                                $data = $this->apilib->runDataProcessing($field['filterref'], 'pre-search');
+                                $where = '1';
+                                foreach ($data as $campo => $valore) {
+                                    if ($campo) {
+                                        $where .= " AND {$campo} = '$valore'";
+                                    } else {
+                                        $where .= " AND $valore";
+                                    }
+                                }
 
-                                            if ($where === '1') {
-                                                $where = null;
-                                            }
-                                            //debug($where);
-                                            ?>
+                                if ($where === '1') {
+                                    $where = null;
+                                }
+                                //debug($where);
+                                ?>
                                 <select class="form-control select2_standard <?php echo $class ?>" data-source-field="<?php echo (!empty($field['fields_source'])) ? $field['fields_source'] : '' ?>" name="conditions[<?php echo $k; ?>][value]" data-ref="<?php echo (!empty($field['fields_ref'])) ? $field['fields_ref'] : '' ?>" data-val="<?php echo $value; ?>" <?php echo $onclick; ?>>
                                     <option value="">---</option>
 
@@ -142,10 +142,10 @@ $where_data = array_combine(array_key_map($_sess_where_data, 'field_id'), $_sess
                             <?php else : ?>
                                 <!--<input type="hidden" name="conditions[<?php echo $k; ?>][value]" data-field-id="<?php echo $field['id']; ?>" class="form-control js_select_ajax_distinct field_<?php echo $field['id']; ?>" value="<?php echo $value; ?>" />-->
                                 <?php
-                                            $field = array_merge($field, $this->db->where('fields_id', $field['id'])->join('entity', '(fields_entity_id = entity_id)', 'LEFT')->get('fields')->row_array());
-                                            //debug($field);
+                                $field = array_merge($field, $this->db->where('fields_id', $field['id'])->join('entity', '(fields_entity_id = entity_id)', 'LEFT')->get('fields')->row_array());
+                                //debug($field);
 
-                                            ?>
+                                ?>
                                 <select class="form-control select2_standard <?php echo $class ?>" name="conditions[<?php echo $k; ?>][value]" data-source-field="<?php echo $field['fields_source'] ?>" data-ref="<?php echo $field['fields_ref'] ?>" data-val="<?php echo $value; ?>" <?php echo $onclick; ?>>
                                     <option value="">---</option>
 
@@ -168,9 +168,9 @@ $where_data = array_combine(array_key_map($_sess_where_data, 'field_id'), $_sess
 
                 <?php if ($where_data) : ?>
                     <input type="hidden" id="clear-filters-<?php echo $form['forms']['forms_id']; ?>" name="clear-filters" value="" />
-                    <input type="button" onclick="javascript:$('#clear-filters-<?php echo $form['forms']['forms_id']; ?>').val('1');$('#form_<?php echo $form['forms']['forms_id']; ?>').trigger('submit');" class="btn red-intense" value="Svuota filtri" />
+                    <input type="button" onclick="javascript:$('#clear-filters-<?php echo $form['forms']['forms_id']; ?>').val('1');$('#form_<?php echo $form['forms']['forms_id']; ?>').trigger('submit');" class="btn red-intense" value="<?php e('Clear filters'); ?>" />
                 <?php endif; ?>
-                <button type="submit" class="btn btn-primary"><?php echo $where_data ? 'Filtra' : 'Aggiorna filtri'; ?></button>
+                <button type="submit" class="btn btn-primary"><?php echo $where_data ? t('Filter') : t('Update filters'); ?></button>
             </div>
         </div>
     </div>
