ed89b18e8bd11bf0bc08c537cfd93b6f88bb5e25
Michael <michael@h2web.it>
2020-04-23 15:09:12 +0200

    Moved var-dumper package to global composer vendor Fixed undefined variable $var in d() & dd() functions

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,7 +6,17 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit79970b80b6b8a8b3407d2aa31e24500f
 {
+    public static $files = array (
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
+        'S' => 
+        array (
+            'Symfony\\Polyfill\\Mbstring\\' => 26,
+            'Symfony\\Component\\VarDumper\\' => 28,
+        ),
         'P' => 
         array (
             'Picqer\\Barcode\\' => 15,
@@ -15,6 +25,14 @@ class ComposerStaticInit79970b80b6b8a8b3407d2aa31e24500f
     );
 
     public static $prefixDirsPsr4 = array (
+        'Symfony\\Polyfill\\Mbstring\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
+        ),
+        'Symfony\\Component\\VarDumper\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/var-dumper',
+        ),
         'Picqer\\Barcode\\' => 
         array (
             0 => __DIR__ . '/..' . '/picqer/php-barcode-generator/src',
