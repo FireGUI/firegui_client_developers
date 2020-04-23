bb4d6789d4ee09a1ca9eb4f207e602a6a544085c
Matteo <>
2020-04-23 11:52:51 +0200

    barcodes completed autoload composer removed vendor from gitignore

--- application/views_adminlte/pages/layouts/grids/static_vertical_barcode.php
+++ application/views_adminlte/pages/layouts/grids/static_vertical_barcode.php
@@ -3,33 +3,40 @@
 
         <div class="col-md-12">
             <?php foreach ($grid['grids_fields'] as $field) : ?>
-                <div class="col-md-6">
+                <div class="col-md-12">
                     <?php
-                    // $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
-                    // echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('081231723897', $generator::TYPE_CODE_128)) . '">';
+                    $field_label = $field['grids_fields_column_name'];
+                    $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
+
+                    foreach ([$generator::TYPE_CODE_128, $generator::TYPE_EAN_13] as $type) :
                     ?>
-                    <barcode type="qr" mode="H">Hello</barcode>
+                        <?php
+                        // debug($type);
+                        // debug($dato[$field['fields_name']]);
+                        switch ($type) {
+                            case $generator::TYPE_EAN_13:
+                                $value = preg_replace('/\D/', '', $dato[$field['fields_name']]);
+                                dd($generator);
+                                break;
+                            default:
+                                $value = $dato[$field['fields_name']];
+                                break;
+                        }
+                        if (!$value) {
+                            continue;
+                        }
+                        ?>
+                        <div>
 
+                            <div class="text-center"><strong><?php echo $type; ?></strong></div>
+                            <div class="text-center"><label><?php echo $field_label; ?></label></div>
 
-                    <barcode code="04210000526" type="UPCE"></barcode>
-                    <!-- Note the UPC-A code is required which is converted to UPCE -->
-
-
-                    <barcode code="978-0-9542246-0-8 07" type="ISSNP2" text="1"></barcode>
-
-
-                    <barcode code="01234567094987654321-01234567891" type="IMB"></barcode>
-
-
-                    <barcode code="SN34RD1A" type="RM4SCC"></barcode>
-
-
-                    <barcode code="54321068" type="I25"></barcode>
-
-
-                    <barcode code="A34698735B" type="CODABAR"></barcode>
-
-
+                            <div class="text-center"><img src="data:image/png;base64,<?php echo (base64_encode($generator->getBarcode($value, $type))); ?>" /><br /></div>
+                            <div class="text-center"><small><?php echo $value; ?></small></div>
+                            <a class="btn btn-default" href="<?php echo base_url('main/print_barcode/' . $type . '/?val=' . base64_encode($value)); ?>" target="_blank"><?php e('Print'); ?></a>
+                        </div>
+                    <?php endforeach; ?>
+                    <hr />
                 </div>
             <?php endforeach; ?>
         </div>
