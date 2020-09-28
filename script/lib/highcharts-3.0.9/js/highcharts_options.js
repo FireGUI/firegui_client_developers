6185475928d7d7d3566afa3948a07ef7e539d406
Matteo <>
2020-09-28 10:45:03 +0200

    External crmnotifier js external highcharts options js

---
+++ script/lib/highcharts-3.0.9/js/highcharts_options.js
@@ -0,0 +1,16 @@
+$(function () {
+    'use strict';
+    Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function (color) {
+        return {
+            radialGradient: {
+                cx: 0.5,
+                cy: 0.3,
+                r: 0.7
+            },
+            stops: [
+                [0, color],
+                [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+            ]
+        };
+    });
+});
