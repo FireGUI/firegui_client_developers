dd07fbdccfc942da65cd1d4842c2566cc0054c8e
Michael <michael@h2web.it>
2020-09-28 14:24:34 +0200

    Moved inline styles to custom ext css

--- application/views_adminlte/box/grid/inline_form_actions.php
+++ application/views_adminlte/box/grid/inline_form_actions.php
@@ -1,6 +1,6 @@
 <div class="action-list">
     <a href="#" class="js_edit btn bg-purple btn-grid-action-s" data-id="<?php echo $id; ?>">
-        <span class="fas fa-edit" style="color:white !important;"></span>
+        <span class="fas fa-edit"></span>
     </a>
 
 
@@ -34,7 +34,7 @@
                     continue;
                 }
                 ?>
-                <span <?php echo $custom_action['grids_actions_name'] ? "data-toggle='tooltip' title='{$custom_action['grids_actions_name']}'" : null; ?> style="color:white !important; margin-left: 3px;"><?php echo $action; ?></span>
+                <span class="custom-action" <?php echo $custom_action['grids_actions_name'] ? "data-toggle='tooltip' title='{$custom_action['grids_actions_name']}'" : null; ?>><?php echo $action; ?></span>
             <?php else : ?>
 
                 <?php
@@ -78,7 +78,7 @@
                 ?>
                 <span <?php echo $custom_action['grids_actions_name'] ? "data-toggle='tooltip' title='{$custom_action['grids_actions_name']}'" : null; ?>>
                     <a class="js-action_button btn btn-grid-action-s btn-primary<?php if ($confirm) : ?> js_confirm_button js_link_ajax<?php endif; ?> <?php if (in_array($custom_action['grids_actions_mode'], ['modal', 'modal_large', 'modal_extra'])) echo 'js_open_modal'; ?>" href="<?php echo $url; ?>" <?php if ($custom_action['grids_actions_mode'] == 'new_tab') : ?>target="_blank" <?php endif; ?>style="background-color: <?php echo ($custom_action['grids_actions_color']) ?: '#CCCCCC'; ?>" <?php if ($confirm) : ?> data-confirm-text="<?php e('Are you sure to delete this record?'); ?>" data-toggle="tooltip" <?php endif; ?>>
-                        <span class="<?php echo $custom_action['grids_actions_icon']; ?>" style="color:white !important;"></span>
+                        <span class="<?php echo $custom_action['grids_actions_icon']; ?>"></span>
                     </a>
                 </span>
 
