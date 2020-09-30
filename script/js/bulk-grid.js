b19832ceab94b36eccb862305fe337dd02d37c77
Michael <michael@h2web.it>
2020-09-30 11:14:56 +0200

    Fix js files double quotes to single

--- script/js/bulk-grid.js
+++ script/js/bulk-grid.js
@@ -1,114 +1,90 @@
-"use strict";
+'use strict';
 $(document).ready(function () {
-  $('.js-bulk-action > option[value=""]')
-    .attr("selected", "selected")
-    .trigger("change");
-  $("input:checkbox.js_bulk_check,.js-bulk-select-all")
-    .removeAttr("checked")
-    .trigger("change");
-  $.uniform.update();
-  $(".js-bulk-select-all").on("click", function () {
-    var grid_container = $(this)
-      .closest("table.table")
-      .closest(".grid,.tab-pane");
-
-    if ($(this).is(":checked")) {
-      $('input[type="checkbox"].js_bulk_check', grid_container)
-        .attr("checked", "checked")
-        .trigger("change");
-    } else {
-      $('input[type="checkbox"].js_bulk_check', grid_container)
-        .removeAttr("checked")
-        .trigger("change");
-    }
-    //Questo ridisegna le checkbox metronic
+    $('.js-bulk-action > option[value=""]').attr('selected', 'selected').trigger('change');
+    $('input:checkbox.js_bulk_check,.js-bulk-select-all').removeAttr('checked').trigger('change');
     $.uniform.update();
+    $('.js-bulk-select-all').on('click', function () {
+        var grid_container = $(this).closest('table.table').closest('.grid,.tab-pane');
 
-    var chkbx_ids = $("input:checkbox.js_bulk_check:checked", grid_container)
-      .map(function () {
-        return $(this).val();
-      })
-      .get();
-
-    if (chkbx_ids.length == 0) {
-      $(".js-bulk-first-option", grid_container).html("");
-      $(".js-bulk-action option", grid_container).attr("disabled", "disabled");
-    } else {
-      $(".js-bulk-first-option", grid_container).html(
-        chkbx_ids.length + " selected"
-      );
-      $(".js-bulk-action option", grid_container).removeAttr("disabled");
-    }
-  });
-
-  $("table").on("click", "input:checkbox.js_bulk_check", function () {
-    //Questo ridisegna le checkbox metronic
-    $.uniform.update();
-    var grid_container = $(this)
-      .closest("table.table")
-      .closest(".grid,.tab-pane");
-    var chkbx_ids = $("input:checkbox.js_bulk_check:checked", grid_container)
-      .map(function () {
-        return $(this).val();
-      })
-      .get();
-    //alert(chkbx_ids.length);
-    if (chkbx_ids.length == 0) {
-      $(".js-bulk-first-option", grid_container).html("");
-      $(".js-bulk-action option", grid_container).attr("disabled", "disabled");
-    } else {
-      $(".js-bulk-first-option", grid_container).html(
-        chkbx_ids.length + " selected"
-      );
-      $(".js-bulk-action option", grid_container).removeAttr("disabled");
-    }
-  });
-
-  $(".js-bulk-action").on("change", function () {
-    var grid_container = $(this).closest("div[data-layout-box]");
-    if ($(this).val() != "") {
-      var chkbx_ids = $("input:checkbox.js_bulk_check:checked", grid_container)
-        .map(function () {
-          return $(this).val();
-        })
-        .get();
-
-      if ($(this).val() == "bulk_edit") {
-        var data_post = [];
-
-        for (var i in chkbx_ids) {
-          data_post.push({ name: "ids[]", value: chkbx_ids[i] });
+        if ($(this).is(':checked')) {
+            $('input[type="checkbox"].js_bulk_check', grid_container).attr('checked', 'checked').trigger('change');
+        } else {
+            $('input[type="checkbox"].js_bulk_check', grid_container).removeAttr('checked').trigger('change');
         }
-        var form_id = $(this).find(":selected").data("form_id");
+        //Questo ridisegna le checkbox metronic
+        $.uniform.update();
 
-        loadModal(
-          base_url + "get_ajax/modal_form/" + form_id,
-          data_post,
-          null,
-          "POST"
-        );
-      } else if ($(this).val() == "bulk_delete") {
-        var r = confirm(
-          "Confermi di voler eliminare " + chkbx_ids.length + " righe?"
-        );
-        if (r == true) {
-          var url =
-            base_url + "db_ajax/generic_delete/" + $(this).data("entity-name");
-          var data_post = [];
-          data_post.push({ name: token_name, value: token_hash });
-          for (var i in chkbx_ids) {
-            data_post.push({ name: "ids[]", value: chkbx_ids[i] });
-          }
-          $.ajax({
-            url: url,
-            type: "POST",
-            data: data_post,
-            success: function (json) {
-              location.reload();
-            },
-          });
+        var chkbx_ids = $('input:checkbox.js_bulk_check:checked', grid_container)
+            .map(function () {
+                return $(this).val();
+            })
+            .get();
+
+        if (chkbx_ids.length == 0) {
+            $('.js-bulk-first-option', grid_container).html('');
+            $('.js-bulk-action option', grid_container).attr('disabled', 'disabled');
+        } else {
+            $('.js-bulk-first-option', grid_container).html(chkbx_ids.length + ' selected');
+            $('.js-bulk-action option', grid_container).removeAttr('disabled');
         }
-      }
-    }
-  });
+    });
+
+    $('table').on('click', 'input:checkbox.js_bulk_check', function () {
+        //Questo ridisegna le checkbox metronic
+        $.uniform.update();
+        var grid_container = $(this).closest('table.table').closest('.grid,.tab-pane');
+        var chkbx_ids = $('input:checkbox.js_bulk_check:checked', grid_container)
+            .map(function () {
+                return $(this).val();
+            })
+            .get();
+        //alert(chkbx_ids.length);
+        if (chkbx_ids.length == 0) {
+            $('.js-bulk-first-option', grid_container).html('');
+            $('.js-bulk-action option', grid_container).attr('disabled', 'disabled');
+        } else {
+            $('.js-bulk-first-option', grid_container).html(chkbx_ids.length + ' selected');
+            $('.js-bulk-action option', grid_container).removeAttr('disabled');
+        }
+    });
+
+    $('.js-bulk-action').on('change', function () {
+        var grid_container = $(this).closest('div[data-layout-box]');
+        if ($(this).val() != '') {
+            var chkbx_ids = $('input:checkbox.js_bulk_check:checked', grid_container)
+                .map(function () {
+                    return $(this).val();
+                })
+                .get();
+
+            if ($(this).val() == 'bulk_edit') {
+                var data_post = [];
+
+                for (var i in chkbx_ids) {
+                    data_post.push({ name: 'ids[]', value: chkbx_ids[i] });
+                }
+                var form_id = $(this).find(':selected').data('form_id');
+
+                loadModal(base_url + 'get_ajax/modal_form/' + form_id, data_post, null, 'POST');
+            } else if ($(this).val() == 'bulk_delete') {
+                var r = confirm('Confermi di voler eliminare ' + chkbx_ids.length + ' righe?');
+                if (r == true) {
+                    var url = base_url + 'db_ajax/generic_delete/' + $(this).data('entity-name');
+                    var data_post = [];
+                    data_post.push({ name: token_name, value: token_hash });
+                    for (var i in chkbx_ids) {
+                        data_post.push({ name: 'ids[]', value: chkbx_ids[i] });
+                    }
+                    $.ajax({
+                        url: url,
+                        type: 'POST',
+                        data: data_post,
+                        success: function (json) {
+                            location.reload();
+                        },
+                    });
+                }
+            }
+        }
+    });
 });
--- script/js/crmNotifier.js
+++ script/js/crmNotifier.js
@@ -1,142 +1,130 @@
 var CrmNotifier = {
-  /**
-   * jQuery object
-   * @type {jQuery}
-   */
-  listContainer: $("#header_notification_bar"),
+    /**
+     * jQuery object
+     * @type {jQuery}
+     */
+    listContainer: $('#header_notification_bar'),
 
-  /**
-   * Number of the unread notifications
-   * @type Number
-   */
-  number: 0,
+    /**
+     * Number of the unread notifications
+     * @type Number
+     */
+    number: 0,
 
-  audio: null,
-  played: false,
+    audio: null,
+    played: false,
 
-  fetch: function () {
-    var notifier = this;
-    $.ajax({
-      url: base_url + "get_ajax/dropdown_notification_list",
-      dataType: "json",
-      success: function (json) {
-        notifier.number = json.count;
-        notifier.showUnread(json.view);
-        if (notifier.number > 0) {
-          var baseSoundFolder = base_url_scripts + "script/js/";
+    fetch: function () {
+        var notifier = this;
+        $.ajax({
+            url: base_url + 'get_ajax/dropdown_notification_list',
+            dataType: 'json',
+            success: function (json) {
+                notifier.number = json.count;
+                notifier.showUnread(json.view);
+                if (notifier.number > 0) {
+                    var baseSoundFolder = base_url_scripts + 'script/js/';
 
-          notifier.playNotificationSound(baseSoundFolder + "notify-sound.mp3");
+                    notifier.playNotificationSound(baseSoundFolder + 'notify-sound.mp3');
+                }
+            },
+        });
+    },
+
+    showUnread: function (html) {
+        if (typeof html === 'string') {
+            $('.js_notification_dropdown_list', this.listContainer).html(html);
         }
-      },
-    });
-  },
 
-  showUnread: function (html) {
-    if (typeof html === "string") {
-      $(".js_notification_dropdown_list", this.listContainer).html(html);
-    }
+        $('.js_notification_number_label', this.listContainer).text(this.number > 0 ? this.number : '');
+        $('.js_notification_number', this.listContainer).text(this.number);
+    },
 
-    $(".js_notification_number_label", this.listContainer).text(
-      this.number > 0 ? this.number : ""
-    );
-    $(".js_notification_number", this.listContainer).text(this.number);
-  },
-
-  playNotificationSound: function (notificationSound) {
-    if (!notificationSound) {
-      alert("There are new notification");
-      return;
-    }
-
-    if (this.isPlayable()) {
-      if (!this.audio) {
-        this.audio = $("<audio/>")
-          .hide()
-          .append($("<source/>"))
-          .appendTo("body");
-      }
-
-      $("source", this.audio).attr("src", notificationSound);
-
-      this.audio[0].play();
-      localStorage.setItem("played", new Date());
-    }
-  },
-
-  isPlayable: function () {
-    var time = localStorage.getItem("played");
-    if (!time) {
-      return true;
-    }
-
-    var diff = new Date() - new Date(time);
-    if (isNaN(diff)) {
-      return true;
-    }
-
-    // Diff è la differenza tra le due date in millisecondi
-    // Se è maggiore di 1500 (25 min) allora è suonabile
-    return diff / 1000 > 1500;
-  },
-
-  setRead: function (notificationId) {
-    if (this.number < 1 && !notificationId) {
-      return null;
-    }
-
-    return $.ajax(
-      base_url +
-        "db_ajax/notify_read/" +
-        (typeof notificationId === "undefined" ? "" : notificationId)
-    );
-  },
-
-  readAll: function () {
-    var ajax = this.setRead();
-    if (ajax !== null) {
-      var notifier = this;
-      ajax.success(function () {
-        notifier.number = 0;
-        notifier.showUnread();
-      });
-    }
-  },
-
-  readAndGoto: function (notificationId, link) {
-    var that = this;
-    var ajax = this.setRead(notificationId);
-    if (ajax !== null) {
-      ajax.success(function () {
-        "use strict";
-        if (link === "javascript:void(0);") {
-          that.fetch();
-        } else {
-          window.location = link;
+    playNotificationSound: function (notificationSound) {
+        if (!notificationSound) {
+            alert('There are new notification');
+            return;
         }
-      });
-    }
-  },
 
-  init: function () {
-    var notifier = this;
-    notifier.fetch();
+        if (this.isPlayable()) {
+            if (!this.audio) {
+                this.audio = $('<audio/>').hide().append($('<source/>')).appendTo('body');
+            }
 
-    setInterval(function () {
-      notifier.fetch();
-    }, 5 * 60 * 1000); // 5 min
-    notifier.listContainer.on("click", "[data-notification]", function (e) {
-      e.preventDefault();
+            $('source', this.audio).attr('src', notificationSound);
 
-      var $this = $(this);
-      notifier.readAndGoto(
-        $this.data("notification"),
-        $("a", $this).attr("href")
-      );
-    });
-  },
+            this.audio[0].play();
+            localStorage.setItem('played', new Date());
+        }
+    },
+
+    isPlayable: function () {
+        var time = localStorage.getItem('played');
+        if (!time) {
+            return true;
+        }
+
+        var diff = new Date() - new Date(time);
+        if (isNaN(diff)) {
+            return true;
+        }
+
+        // Diff è la differenza tra le due date in millisecondi
+        // Se è maggiore di 1500 (25 min) allora è suonabile
+        return diff / 1000 > 1500;
+    },
+
+    setRead: function (notificationId) {
+        if (this.number < 1 && !notificationId) {
+            return null;
+        }
+
+        return $.ajax(base_url + 'db_ajax/notify_read/' + (typeof notificationId === 'undefined' ? '' : notificationId));
+    },
+
+    readAll: function () {
+        var ajax = this.setRead();
+        if (ajax !== null) {
+            var notifier = this;
+            ajax.success(function () {
+                notifier.number = 0;
+                notifier.showUnread();
+            });
+        }
+    },
+
+    readAndGoto: function (notificationId, link) {
+        var that = this;
+        var ajax = this.setRead(notificationId);
+        if (ajax !== null) {
+            ajax.success(function () {
+                'use strict';
+                if (link === 'javascript:void(0);') {
+                    that.fetch();
+                } else {
+                    window.location = link;
+                }
+            });
+        }
+    },
+
+    init: function () {
+        var notifier = this;
+        notifier.fetch();
+
+        setInterval(function () {
+            notifier.fetch();
+        }, 5 * 60 * 1000); // 5 min
+        notifier.listContainer.on('click', '[data-notification]', function (e) {
+            e.preventDefault();
+
+            var $this = $(this);
+            notifier.readAndGoto($this.data('notification'), $('a', $this).attr('href'));
+        });
+    },
 };
 
 $(document).ready(function () {
-  "use strict";
-  CrmNotifier.init();
+    'use strict';
+    CrmNotifier.init();
 });
