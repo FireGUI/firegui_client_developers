60cb7918cf1e759b80798953b704baaf83d6f1c6
Michael <michael@h2web.it>
2020-10-16 16:28:25 +0200

    Fix https per nginx Aggiunto if not cli

--- application/config/config.php
+++ application/config/config.php
@@ -24,11 +24,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 
-$protocol = (!empty($_SERVER['HTTPS'])  && $_SERVER['HTTPS'] !== 'off' || (!empty($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443)) ? "https://" : "http://";
+$base_url = '';
 
-$base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-$base_url .= "://" . ((!empty($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : '');
-$base_url .=     str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+if (!is_cli()) {
+    $base_url = ((!empty($_SERVER['HTTPS'])  && $_SERVER['HTTPS'] !== 'off') || (!empty($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443)) ? "https" : "http";
+    $base_url .= "://" . ((!empty($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : '');
+    $base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+}
+
 $config['base_url'] = $base_url;
 
 
