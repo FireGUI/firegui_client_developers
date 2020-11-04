df62495802770c68c139b9bdcc518c286f025a1e
Michael <michael@h2web.it>
2020-11-04 11:57:09 +0100

    Added custom ranges to daterangepicker component

--- script/js/components.js
+++ script/js/components.js
@@ -75,6 +75,14 @@ function initComponents(container) {
                     format: 'DD/MM/YYYY',
                     firstDay: 1,
                 },
+                ranges: {
+                    'Oggi': [moment(), moment()],
+                    'Ieri': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Ultimi 7 Giorni': [moment().subtract(6, 'days'), moment()],
+                    'Ultimi 30 Giorni': [moment().subtract(29, 'days'), moment()],
+                    'Mese corrente': [moment().startOf('month'), moment().endOf('month')],
+                    'Mese precedente': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                },
             },
             function (start, end) {
                 $('input', this.element).val(start.format('DD/MM/YYYY') + ' - ' + end.format('DD/MM/YYYY'));
