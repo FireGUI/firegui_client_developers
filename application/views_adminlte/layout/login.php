2f2e61af5f1f050d79e8adc95bd603f71da3fdb0
Andrea <andrea@h2web.it>
2021-06-07 16:33:02 +0200

    Added new form in login page Removed old login form from easylogin page

--- application/views_adminlte/layout/login.php
+++ application/views_adminlte/layout/login.php
@@ -49,7 +49,7 @@ if (file_exists(VIEWPATH . 'custom/layout/login.php')) {
 
         <!-- CORE LEVEL STYLES -->
         <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css?v={$this->config->item('version')}"); ?>" />
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/bower_components/font-awesome/css/font-awesome.min.css?v={$this->config->item('version')}"); ?>" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/bower_components/@fortawesome/fontawesome-free/css/all.min.css?v=" . VERSION); ?>" />
         <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/bower_components/Ionicons/css/ionicons.min.css?v={$this->config->item('version')}"); ?>" />
 
         <link rel="stylesheet" type="text/css" href="<?php echo base_url_template("template/adminlte/dist/css/AdminLTE.min.css?v={$this->config->item('version')}"); ?>" />
@@ -97,6 +97,131 @@ if (file_exists(VIEWPATH . 'custom/layout/login.php')) {
         }
         $this->layout->addDinamicStylesheet($data, "login.css");
         ?>
+
+
+        <style>
+            /* Media query per login box width responsive */
+            @media (max-width: 768px) {
+
+                .login-box-security {
+                    width: 90% !important;
+                    margin-top: 20px;
+                }
+            }
+
+            .login-box-security {
+                width: 550px;
+            }
+
+            .login_container {
+                /*min-width: 450px;*/
+                background: #ffffff;
+                padding: 20px 30px;
+                border-radius: 3px;
+            }
+
+
+            .login_logo {
+                width: 100%;
+                height: 45px;
+                display: flex;
+                justify-content: center;
+            }
+
+            .login_logo i {
+                color: #3c8dbc;
+                font-size: 36px;
+            }
+
+            .login_content .login_heading {
+                font-weight: 600;
+                font-size: 2.2rem;
+                color: #000000;
+            }
+
+            .login_content .login_text {
+                font-size: 1.5rem;
+                color: #000000;
+            }
+
+            .login_actions {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: column;
+                margin-top: 30px
+            }
+
+            .login_actions .main_actions {
+                width: 100%;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                margin-bottom: 20px
+            }
+
+            @media (max-width: 768px) {
+                .login_actions .main_actions {
+                    width: 100%;
+                    display: flex;
+                    justify-content: flex-start;
+                    align-items: center;
+                    margin-bottom: 20px;
+                    flex-direction: column;
+                }
+
+                .login_actions .main_actions input {
+                    width: 100% !important;
+                    margin-bottom: 15px;
+                }
+            }
+
+            .login_actions .main_actions input {
+                width: 45%;
+            }
+
+            .login_actions .main_actions .js_easylogin_proceed {
+                border: 0;
+                background: #3c8dbc;
+                color: #ffffff;
+                font-size: 1.5rem;
+                font-weight: 600;
+                padding: 10px 15px;
+                transition: all 0.25s ease-in;
+            }
+
+            .login_actions .main_actions .js_easylogin_proceed:hover {
+                background: #367fa9;
+            }
+
+            .login_actions .main_actions .js_easylogin_later {
+                background: #ffffff;
+                border: 1px solid #3c8dbc;
+                color: #3c8dbc;
+                font-size: 1.5rem;
+                font-weight: 600;
+                padding: 10px 15px;
+                transition: all 0.25s ease-in;
+            }
+
+            .login_actions .main_actions .js_easylogin_later:hover {
+                background: #3c8dbc;
+                color: #ffffff;
+            }
+
+            .login_actions .last_action .js_easylogin_never {
+                background: #ffffff;
+                border: 0;
+                color: #3c8dbc;
+                font-weight: 600;
+                padding: 5px 10px;
+                transition: all 0.25s ease-in;
+            }
+
+            .login_actions .last_action .js_easylogin_never:hover {
+                color: #367fa9;
+            }
+        </style>
 
 
     </head>
@@ -195,6 +320,50 @@ if (file_exists(VIEWPATH . 'custom/layout/login.php')) {
                         </div>
 
                     <?php endif; ?>
+                </div>
+            </div>
+
+            <!-- New login -->
+            <div class="login-box login-box-security">
+                <div class="logo">
+                    <div class="text-center">
+                        <?php if ($this->settings === array()) : ?>
+                            <h2 class="login-logo"><?php e('La tua azienda'); ?></h2>
+                        <?php elseif ($this->settings['settings_company_logo']) : ?>
+                            <img src="<?php echo base_url_uploads("uploads/{$this->settings['settings_company_logo']}"); ?>" alt="logo" class="logo" />
+                        <?php else : ?>
+                            <h2 class=" text-danger"><?php echo $this->settings['settings_company_short_name']; ?></h2>
+                        <?php endif; ?>
+                    </div>
+                </div>
+
+                <div class="login-box-body">
+                    <div class="login_container">
+                        <div class="login_logo">
+                            <i class="fas fa-fingerprint"></i>
+                        </div>
+                        <div class="login_content">
+                            <p class="login_heading text-center">
+                                <?php echo e('Are you tired of passwords?'); ?>
+                            </p>
+                            <p class="login_text text-center">
+                                <?php echo e('Depending on your device you will be able to login with your fingerprint, face recognition or PIN.'); ?>
+                            </p>
+                        </div>
+                        <div class="login_actions">
+                            <div class="main_actions">
+                                <input type="button" class="js_easylogin_later" value="<?php echo e('Later'); ?>" />
+                                <input type="button" class="js_easylogin_proceed" value="<?php echo e('Proceed'); ?> " />
+                            </div>
+                            <div class="last_action">
+                                <input type="button" class="js_easylogin_never" value="<?php echo e('Don\'t ask me again'); ?>" />
+                            </div>
+                        </div>
+                    </div>
+                    <!--<input type="button" class="js_easylogin_later" value="Later..." />
+                    <input type="button" class="js_easylogin_proceed" value="Proceed..." />
+                    <br />
+                    <input type="button" class="js_easylogin_never" value="Don't ask me again..." />-->
                 </div>
             </div>
         </div>
