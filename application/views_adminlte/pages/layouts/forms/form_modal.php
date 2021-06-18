b9409bda95c8f80161a8f738e6f9f1b2124bbea3
Matteo Puppis <matteo@h2web.it>
2021-06-18 15:12:18 +0200

    Fix row class in form modal

--- application/views_adminlte/pages/layouts/forms/form_modal.php
+++ application/views_adminlte/pages/layouts/forms/form_modal.php
@@ -1,6 +1,9 @@
 <?php
 $form_id = "form_{$form['forms']['forms_id']}";
 $bulk_mode = (is_array($value_id));
+$rowStart = '<div class="row">';
+$rowEnd = '</div>';
+$rowCol = 0;
 ?>
 
 <?php
@@ -31,65 +34,80 @@ $show_delete_button = ($form['forms']['forms_show_delete'] == DB_BOOL_TRUE && $v
                 <h4 class="modal-title" id="myModalLabel"><?php echo t(ucwords(str_replace('_', ' ', $form['forms']['forms_name']))); ?></h4>
             </div>
             <div class="modal-body">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-xs-12">
 
-                <div class="row">
-                    <div class="col-xs-12">
+                            <form <?php echo "id='{$form_id}'"; ?> role="form" method="post" action="<?php echo $form['forms']['action_url']; ?>" class="form formAjax" enctype="multipart/form-data" <?php if (!is_array($value_id)) : ?>data-edit-id="<?php echo $value_id; ?>" <?php else : ?>data-edit-id="" <?php endif; ?>>
+                                <?php add_csrf(); ?>
+                                <?php if ($bulk_mode) : ?>
+                                    <?php foreach ($value_id as $val) : ?>
+                                        <input type="hidden" name="value_id[]" value="<?php echo $val; ?>" />
+                                    <?php endforeach; ?>
+                                <?php endif; ?>
+                                <!-- FORM HIDDEN DATA -->
+                                <?php echo implode(PHP_EOL, $form['forms_hidden']); ?>
 
-                        <form <?php echo "id='{$form_id}'"; ?> role="form" method="post" action="<?php echo $form['forms']['action_url']; ?>" class="form formAjax" enctype="multipart/form-data" <?php if (!is_array($value_id)) : ?>data-edit-id="<?php echo $value_id; ?>" <?php else : ?>data-edit-id="" <?php endif; ?>>
-                            <?php add_csrf(); ?>
-                            <?php if ($bulk_mode) : ?>
-                                <?php foreach ($value_id as $val) : ?>
-                                    <input type="hidden" name="value_id[]" value="<?php echo $val; ?>" />
-                                <?php endforeach; ?>
-                            <?php endif; ?>
-                            <!-- FORM HIDDEN DATA -->
-                            <?php echo implode(PHP_EOL, $form['forms_hidden']); ?>
+                                <div class="form-body">
+                                    <div class="row">
+                                        <?php foreach ($form['forms_fields'] as $field) : ?>
+                                            <?php
+                                            // Stampa la prima row
+                                            echo $rowCol ? '' : $rowStart;
+                                            $col = $field['size'] ?: 6;
+                                            $rowCol += $col;
 
-                            <div class="form-body">
-                                <div class="row">
-                                    <?php foreach ($form['forms_fields'] as $field) : ?>
-                                        <?php if ($bulk_mode) : ?>
-                                            <div class="js_field_container">
-                                                <div class="col-lg-3">
-                                                    <label>Edit this:</label>
-                                                    <input type="checkbox" class="_form-control js_field_check" name="edit_fields[]" value="<?php echo $field['name']; ?>" />
+                                            //debug($rowCol);
+
+                                            if ($rowCol > 12) {
+                                                $rowCol = $col;
+                                                echo $rowEnd, $rowStart;
+                                            }
+                                            ?>
+                                            <?php if ($bulk_mode) : ?>
+                                                <div class="js_field_container">
+                                                    <div class="col-lg-3">
+                                                        <label>Edit this:</label>
+                                                        <input type="checkbox" class="_form-control js_field_check" name="edit_fields[]" value="<?php echo $field['name']; ?>" />
+                                                    </div>
+                                                    <div class="col-lg-9">
+                                                    <?php else : ?>
+                                                        <div class="<?php echo sprintf('col-md-%d', $field['size'] ?: 12); ?>">
+                                                        <?php endif; ?>
+                                                        <?php echo $field['html']; ?>
+
+                                                        </div>
+                                                        <?php if ($bulk_mode) : ?>
+                                                    </div><?php endif; ?>
+                                            <?php endforeach; ?>
                                                 </div>
-                                                <div class="col-lg-9">
-                                                <?php else : ?>
-                                                    <div class="<?php echo sprintf('col-md-%d', $field['size'] ?: 12); ?>">
+
+                                                <div class="row">
+                                                    <div class="col-md-12">
+                                                        <div <?php echo "id='msg_{$form_id}'"; ?> class="alert alert-danger hide"></div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-actions">
+                                                    <?php if ($show_delete_button) : ?>
+                                                        <div class="pull-left">
+                                                            <a href="<?php echo base_url("db_ajax/generic_delete/{$form['forms']['entity_name']}/$value_id"); ?>" data-confirm-text="<?php e('Are you sure to delete this record?'); ?>" class="btn btn-danger js_confirm_button js_link_ajax " data-toggle="tooltip" title="" data-original-title="Elimina">
+                                                                <?php e('Delete'); ?>
+                                                            </a>
+
+                                                        </div>
                                                     <?php endif; ?>
-                                                    <?php echo $field['html']; ?>
-
+                                                    <div class="pull-right">
+                                                        <button type="button" class="btn btn-default " data-dismiss="modal"><?php e('Cancel'); ?></button>
+                                                        <button type="submit" class="btn btn-primary"><?php e('Save'); ?></button>
                                                     </div>
-                                                    <?php if ($bulk_mode) : ?></div><?php endif; ?>
-                                        <?php endforeach; ?>
-                                            </div>
-
-                                            <div class="row">
-                                                <div class="col-md-12">
-                                                    <div <?php echo "id='msg_{$form_id}'"; ?> class="alert alert-danger hide"></div>
                                                 </div>
-                                            </div>
+                                    </div>
 
-                                            <div class="form-actions">
-                                                <?php if ($show_delete_button) : ?>
-                                                    <div class="pull-left">
-                                                        <a href="<?php echo base_url("db_ajax/generic_delete/{$form['forms']['entity_name']}/$value_id"); ?>" data-confirm-text="<?php e('Are you sure to delete this record?'); ?>" class="btn btn-danger js_confirm_button js_link_ajax " data-toggle="tooltip" title="" data-original-title="Elimina">
-                                                            <?php e('Delete'); ?>
-                                                        </a>
+                            </form>
+                        </div>
 
-                                                    </div>
-                                                <?php endif; ?>
-                                                <div class="pull-right">
-                                                    <button type="button" class="btn btn-default " data-dismiss="modal"><?php e('Cancel'); ?></button>
-                                                    <button type="submit" class="btn btn-primary"><?php e('Save'); ?></button>
-                                                </div>
-                                            </div>
-                                </div>
-
-                        </form>
                     </div>
-
                 </div>
             </div>
         </div>
