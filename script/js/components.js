9491ca49f5dd5b116f9f163515aecb38c6da9eae
Matteo Puppis <matteo@h2web.it>
2021-08-12 17:53:46 +0200

    Removed fadeout effect

--- script/js/components.js
+++ script/js/components.js
@@ -17,13 +17,13 @@ $('body').on('click', '.js_ajax_content', function (e) {
     var that = $(this);
     if (layout_id && !e.metaKey) {
         e.preventDefault();
-        $('#js_page_content').fadeOut();
+        //$('#js_page_content').fadeOut();
         $.ajax(base_url + 'main/get_layout_content/' + layout_id + get_params, {
             type: 'GET',
             dataType: 'json',
-            complete: function () { $('#js_page_content').show(); },
+            //complete: function () { $('#js_page_content').show(); },
             success: function (data) {
-                $('#js_page_content').show();
+                //$('#js_page_content').show();
                 if (data.status == 0) {
                     console.log(data.msg);
                 }
