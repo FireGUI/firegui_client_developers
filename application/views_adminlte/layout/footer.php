8eca20ca08a3ac938ecf816d174572c212305371
Michael <michael@h2web.it>
2021-08-26 12:39:32 +0200

    update calendar_full_json to fullcalendar-4 removed useless assets

--- application/views_adminlte/layout/footer.php
+++ application/views_adminlte/layout/footer.php
@@ -83,16 +83,9 @@
 <script src="<?php echo base_url_template("template/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js?v=" . VERSION); ?>"></script>
 
 <!-- Morris.js charts -->
-<script src="<?php echo base_url_template("template/adminlte/bower_components/raphael/raphael.min.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_template("template/adminlte/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_template("template/adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_template("template/adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_template("template/adminlte/bower_components/jquery-knob/dist/jquery.knob.min.js?v=" . VERSION); ?>"></script>
 <script src="<?php echo base_url_template("template/adminlte/bower_components/moment/min/moment.min.js?v=" . VERSION); ?>"></script>
 <script src="<?php echo base_url_template("template/adminlte/bower_components/bootstrap-daterangepicker/daterangepicker.js?v=" . VERSION); ?>"></script>
 <script src="<?php echo base_url_template("template/adminlte/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_template("template/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_template("template/adminlte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js?v=" . VERSION); ?>"></script>
 <script src="<?php echo base_url_template("template/adminlte/bower_components/fastclick/lib/fastclick.js?v=" . VERSION); ?>"></script>
 <script src="<?php echo base_url_template("template/adminlte/dist/js/adminlte.min.js?v=" . VERSION); ?>"></script>
 
@@ -100,9 +93,13 @@
 <script src="<?php echo base_url_template("template/adminlte/bower_components/datatables.net/js/jquery.dataTables.min.js?v=" . VERSION); ?>"></script>
 <script src="<?php echo base_url_template("template/adminlte/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js?v=" . VERSION); ?>"></script>
 
+<!-- Full Calendar 4 -->
+<script src='<?php echo base_url_scripts('script/lib/fullcalendar-4/core/main.js') ?>'></script>
+<script src='<?php echo base_url_scripts('script/lib/fullcalendar-4/interaction/main.js') ?>'></script>
+<script src='<?php echo base_url_scripts('script/lib/fullcalendar-4/daygrid/main.js') ?>'></script>
+<script src='<?php echo base_url_scripts('script/lib/fullcalendar-4/timegrid/main.js') ?>'></script>
+
 <!-- INPUT DATA PLUGINS -->
-<script src="<?php echo base_url_scripts("script/global/plugins/fullcalendar/fullcalendar.min.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/global/plugins/fullcalendar/lang-all.js?v=" . VERSION); ?>"></script>
 <script src="<?php echo base_url_scripts("script/global/plugins/jquery.blockui.min.js?v=" . VERSION); ?>"></script>
 <script src="<?php echo base_url_scripts("script/global/plugins/jquery.cokie.min.js?v=" . VERSION); ?>"></script>
 <script src="<?php echo base_url_scripts("script/global/plugins/uniform/jquery.uniform.min.js?v=" . VERSION); ?>"></script>
@@ -131,41 +128,6 @@
 <script src="<?php echo base_url_scripts("script/js/leaflet.geocoding.js?v=" . VERSION); ?>"></script>
 
 <script src="<?php echo base_url_scripts("script/js/leaflet.markercluster.min.js?v=" . VERSION); ?>"></script>
-
-<!-- LEAFLET-DRAW-LIBRARY-JS !-->
-<?php /*
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/Leaflet.draw.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/Leaflet.Draw.Event.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/Toolbar.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/Tooltip.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/ext/GeometryUtil.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/ext/LatLngUtil.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/ext/LineUtil.Intersect.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/ext/Polygon.Intersect.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/ext/Polyline.Intersect.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/ext/TouchEvents.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/draw/DrawToolbar.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/draw/handler/Draw.Feature.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/draw/handler/Draw.SimpleShape.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/draw/handler/Draw.Polyline.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/draw/handler/Draw.Marker.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/draw/handler/Draw.Circle.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/draw/handler/Draw.CircleMarker.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/draw/handler/Draw.Polygon.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/draw/handler/Draw.Rectangle.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/edit/EditToolbar.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/edit/handler/EditToolbar.Edit.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/edit/handler/EditToolbar.Delete.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/Control.Draw.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/edit/handler/Edit.Poly.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/edit/handler/Edit.SimpleShape.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/edit/handler/Edit.Rectangle.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/edit/handler/Edit.Marker.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/edit/handler/Edit.CircleMarker.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-1.7/Leaflet.draw/src/edit/handler/Edit.Circle.js?v=" . VERSION); ?>"></script>
-<script src="<?php echo base_url_scripts("script/lib/leaflet-fullscreen/Leaflet.fullscreen.min.js?v=" . VERSION); ?>"></script>
-
-*/ ?>
 
 <script src="<?php echo base_url_template('script/global/plugins/dropzone/dropzone.js'); ?>"></script>
 
