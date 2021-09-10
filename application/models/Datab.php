8ff006ba3bc905bf7d919ffe4675e84e30262a03
Michael <michael@h2web.it>
2021-09-10 12:00:52 +0200

    Fix undefined variable in getPermission method in datab

--- application/models/Datab.php
+++ application/models/Datab.php
@@ -1826,7 +1826,7 @@ class Datab extends CI_Model
         $this->fixPermissions();
     }
 
-    public function getPermission($userOrGroup)
+    public function getPermission($userOrGroup, $throwException = false)
     {
 
         if (is_numeric($userOrGroup)) {
