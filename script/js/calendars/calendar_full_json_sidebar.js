c8b5d84186f8fc31221ee001e7968012a114a869
Michael <michael@h2web.it>
2021-11-08 16:49:26 +0100

    fix full calendar json sidebar date filter

--- script/js/calendars/calendar_full_json_sidebar.js
+++ script/js/calendars/calendar_full_json_sidebar.js
@@ -1,7 +1,7 @@
 function initCalendars() {
-    $(function () {
+    $(function() {
         'use strict';
-        $('.calendar_full_json_sidebar').each(function () {
+        $('.calendar_full_json_sidebar').each(function() {
             var jqCalendarView;
 
             var jqCalendar = $(this);
@@ -27,7 +27,7 @@ function initCalendars() {
             var m = date.getMonth();
             var y = date.getFullYear();
 
-            var updateCalendar = function (evt) {
+            var updateCalendar = function(evt) {
                 var allDay = isAlldayEvent(evt.event.start, evt.event.end);
                 var fStart = moment(evt.event.start).format('DD/MM/YYYY HH:mm'); // formatted start
                 var fEnd = moment(evt.event.end).format('DD/MM/YYYY HH:mm'); // formatted end
@@ -36,7 +36,7 @@ function initCalendars() {
                     [fieldid]: evt.event.id,
                     [startField]: fStart,
                     [endField]: fEnd
-                    //TODO: manage all days events
+                        //TODO: manage all days events
                 };
 
                 $.ajax({
@@ -44,13 +44,13 @@ function initCalendars() {
                     type: 'POST',
                     dataType: 'json',
                     data: data,
-                    success: function (data) {
+                    success: function(data) {
                         if (parseInt(data.status) < 1) {
                             // revertFunc();
                             alert(data.txt);
                         }
                     },
-                    error: function () {
+                    error: function() {
                         // revertFunc();
                         alert('There was an error while saving the event');
                     },
@@ -93,7 +93,7 @@ function initCalendars() {
                 //     });
                 // },
                 selectHelper: true,
-                select: function (start, end, allDay) {
+                select: function(start, end, allDay) {
                     var fStart = moment(date.start).format('DD/MM/YYYY HH:mm'); // formatted start
                     var fEnd = moment(date.end).format('DD/MM/YYYY HH:mm'); // formatted end
                     var allDay = isAlldayEvent(fStart, fEnd, 'DD/MM/YYYY HH:mm');
@@ -101,10 +101,10 @@ function initCalendars() {
                         [token_name]: token_hash,
                         [startField]: fStart,
                         [endField]: fEnd
-                        //TODO: manage all days events
+                            //TODO: manage all days events
                     };
 
-                    loadModal(formurl, data, function () {
+                    loadModal(formurl, data, function() {
                         calendar.refetchEvents();
                     }, 'get');
 
@@ -113,24 +113,24 @@ function initCalendars() {
                         end.minutes(end.minutes() - 1);
                     }
                 },
-                eventClick: function (evt, jsEvent, view) {
-                    loadModal(formedit + '/' + evt.event.id, {}, function () {
+                eventClick: function(evt, jsEvent, view) {
+                    loadModal(formedit + '/' + evt.event.id, {}, function() {
                         calendar.refetchEvents();
                     });
                     return false;
                 },
 
-                eventDrop: function (evt) {
+                eventDrop: function(evt) {
                     updateCalendar(evt);
                 },
 
-                eventResize: function (evt, delta, revertFunc) {
+                eventResize: function(evt, delta, revertFunc) {
                     updateCalendar(evt);
                 },
                 eventSources: [{
-                    events: function (fetchInfo, successCallback, failureCallback) {
+                    events: function(fetchInfo, successCallback, failureCallback) {
                         var values = [];
-                        $('.js_check_filter').filter('[type=checkbox]:checked').each(function () {
+                        $('.js_check_filter').filter('[type=checkbox]:checked').each(function() {
                             values.push($(this).val());
                         });
 
@@ -140,15 +140,17 @@ function initCalendars() {
                             dataType: 'json',
                             data: {
                                 filters: values,
-                                [token_name]: token_hash
+                                [token_name]: token_hash,
+                                "start": moment(fetchInfo.start).format('YYYY-MM-DD HH:mm'),
+                                "end": moment(fetchInfo.end).format('YYYY-MM-DD HH:mm')
                             },
-                            loading: function (bool) {
+                            loading: function(bool) {
                                 $('#loading').fadeTo(bool ? 1 : 0);
                             },
-                            success: function (response) {
+                            success: function(response) {
                                 successCallback(response);
                             },
-                            error: function (response) {
+                            error: function(response) {
                                 console.log(response);
                                 failureCallback(response);
                             },
@@ -157,7 +159,7 @@ function initCalendars() {
                     color: '#4B8DF8', // a non-ajax option
                     textColor: 'white' // a non-ajax option
                 }],
-                viewRender: function (view) {
+                viewRender: function(view) {
                     // window.sessionStorage.setItem(sessionStorageKey, JSON.stringify({
                     //     view: view.name,
                     //     date: jqCalendar.fullCalendar('getDate').toISOString()
@@ -167,7 +169,7 @@ function initCalendars() {
 
             calendar.render();
 
-            $('.js_check_filter').on('change', function () {
+            $('.js_check_filter').on('change', function() {
                 calendar.refetchEvents();
             });
 
