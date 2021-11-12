ff0963d1154c6577f621d062917586273456c4f4
Michael <michael@h2web.it>
2021-11-12 15:12:25 +0100

    fix tinymce removed deprecated plugin option

--- script/js/components.js
+++ script/js/components.js
@@ -130,7 +130,7 @@ function initComponents(container, reset = false) {
         height: 700,
         autosave_ask_before_unload: false,
         powerpaste_allow_local_images: true,
-        plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount textpattern noneditable charmap quickbars emoticons',
+        plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor insertdatetime advlist lists wordcount textpattern noneditable charmap quickbars emoticons',
         menubar: 'file edit view insert format tools table',
         toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
         toolbar_sticky: true,
