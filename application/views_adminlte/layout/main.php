1241fe91406341e8da889efaaaac32b72172f062
manuelh2web <manuel.aiello@gmail.com>
2021-12-16 12:51:26 +0100

    Remove builder toolbar

--- application/views_adminlte/layout/main.php
+++ application/views_adminlte/layout/main.php
@@ -48,55 +48,6 @@
         <?php echo $footer; ?>
     </div>
 
-
-    <!-- Vertical toolbar -->
-    <div id="builder_toolbar" style="display:none">
-        <?php if ($this->auth->is_admin()) : ?>
-            <div class="material-switch">
-                <input id="js_toolbar_maintenance" type="checkbox" <?php if (is_maintenance()) : ?>checked="checked" <?php endif; ?> value="1">
-                <label for="js_toolbar_maintenance" class="label-success" data-toggle="tooltip" data-placement="bottom" data-container="body" title="Maintenance mode"></label>
-            </div>
-
-            <div class="btn-toolbar">
-                <div class="btn-group-horizontal" role="group" aria-label="...">
-
-                    <button id="js_toolbar_vblink" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" data-container="body" title="Open layout on Visual Builder">
-                        <span class="fas fa-external-link-alt"></span></button>
-
-                    <button id="js_toolbar_vbframe" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" data-container="body" title="Open on Frame Builder">
-                        <span class="fas fa-tv"></span></button>
-
-                    <button id="js_toolbar_highlighter" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" data-container="body" title="Highlight elements"><span class="fas fa-highlighter"></span></button>
-
-                    <button id="js_toolbar_console" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" data-container="body" title="Dev Console"><span class="fas fa-terminal"></span></button>
-
-                    <button id="js_toolbar_download_dump" class="btn btn-default btn-spaced" data-toggle="tooltip" data-placement="bottom" data-container="body" title="Download Dump"><span class="fas fa-download"></span></button>
-                    <button id="js_toolbar_download_zip" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" data-container="body" title="Download Full Zip"><span class="fas fa-cloud-download-alt"></span></button>
-                    <!--
-                    <button id="js_toolbar_backup" class="btn btn-default" data-toggle="tooltip" data-placement="left" data-container="body" title="Backup & Restore"><span class="fas fa-download"></span></button>
-                    <button id="js_toolbar_query" class="btn btn-default" data-toggle="tooltip" data-placement="left" data-container="body" title="Query"><span class="fas fa-pen"></span></button>
-                    -->
-                    <button id="js_toolbar_exit" class="btn btn-default btn-spaced" data-toggle="tooltip" data-placement="bottom" data-container="body" title="Close Toolbar"><span class="fas fa-sign-out-alt"></span></button>
-                </div>
-
-                <!-- Dropdown  
-               <div class="btn-group open">
-                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
-                    <span class="fas fa-cogs"></span>
-                    <span class="sr-only">Toggle Dropdown</span>
-                </button>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">Download Dump</a></li>
-                    <li><a href="#">Download Full Zip</a></li>
-
-                    <li class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                </ul>
-            </div>-->
-            </div>
-        <?php endif; ?>
-    </div>
-
     <div id="builderFrameWrapper">
         <iframe id="builderFrame" src=""></iframe>
     </div>
