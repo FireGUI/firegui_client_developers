6ed0bc8c7cc0709b07040728d346c33588bf7376
Michael <michael@h2web.it>
2022-02-03 10:57:46 +0100

    fix barcode grid multiple barcodes

--- application/views_adminlte/pages/layouts/grids/static_vertical_barcode.php
+++ application/views_adminlte/pages/layouts/grids/static_vertical_barcode.php
@@ -13,66 +13,63 @@
                         continue;
                     }
 
-                    foreach ([$generator::TYPE_CODE_128, $generator::TYPE_EAN_13] as $type) :
+                    $type = $generator::TYPE_EAN_13;
+
+                    $barcodes = json_decode($dato[$field['fields_name']]);
+
+                    //     $barcode_img = base64_encode($generator->getBarcode($barcode, $type));
                     ?>
-                        <?php
-                        switch ($type) {
-                            case $generator::TYPE_EAN_13:
-                                $value = preg_replace('/\D/', '', $dato[$field['fields_name']]);
-                                $value_no_check = $value; //substr($value, 0, 12);
-                                break;
-                            default:
-                                $value = $dato[$field['fields_name']];
-                                $value_no_check = $value;
-                                break;
-                        }
-                        if (!$value) {
-                            continue;
-                        }
-                        $base64img = base64_encode($generator->getBarcode($value_no_check, $type));
-                        ?>
-                        <div class="text-center js_barcode_container" data-type="<?php echo $type; ?>" data-value="<?php echo base64_encode($value); ?>" data-url="main/print_barcode/">
 
-                            <div><strong><?php echo $type; ?></strong></div>
-                            <div><label><?php e($field_label); ?></label></div>
+                    <table class="table table-condensed">
+                        <tbody>
+                            <?php foreach ($barcodes as $barcode) : ?>
+                                <tr>
+                                    <td><?php echo $barcode; ?></td>
+                                    <td><button type="button" class="btn btn-sm btn-info btn-view" data-barcode="<?php echo $barcode; ?>" data-barcode_b64="<?php echo base64_encode($barcode); ?>" data-barcode_img="data:image/png;base64,<?php echo base64_encode($generator->getBarcode($barcode, $type)); ?>"><i class="fas fa-eye fa-fw"></i></button></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
 
-                            <div><img src="data:image/png;base64,<?php echo ($base64img); ?>" /><br /></div>
-                            <div><small><?php echo $value; ?></small></div>
+                    <div class="text-center js_barcode_container hide" data-type="<?php echo $type; ?>" data-value="" data-url="main/print_barcode/">
 
-                            <div class="row">
-                                <div class="col-md-12 ">
-                                    Misure:
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>w:</label>
-                                        <input class="form_control" type="text" name="w" size="3" /> mm
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>h:</label>
-                                        <input class="form_control" type="text" name="h" size="3" /> mm
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>left:</label>
-                                        <input class="form_control" type="text" name="left" size="3" /> mm
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>top:</label> <input class="form_control" type="text" name="top" size="3" /> mm
-                                    </div>
-                                </div>
+                        <div><strong><?php echo $type; ?></strong></div>
 
-                                <a class="btn btn-default js_print" href="javascript:void(0);"><?php e('Print'); ?></a>
+                        <div><img class="barcode_img" /><br /></div>
+                        <div><small class="barcode_label"></small></div>
+
+                        <div class="row">
+                            <div class="col-md-12 ">
+                                Misure:
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>w:</label>
+                                    <input class="form_control" type="text" name="w" size="3" /> mm
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>h:</label>
+                                    <input class="form_control" type="text" name="h" size="3" /> mm
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>left:</label>
+                                    <input class="form_control" type="text" name="left" size="3" /> mm
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>top:</label> <input class="form_control" type="text" name="top" size="3" /> mm
+                                </div>
                             </div>
 
+                            <a class="btn btn-default js_print" href="javascript:void(0);"><?php e('Print'); ?></a>
                         </div>
-                    <?php endforeach; ?>
-                    <hr />
+                    </div>
+
                 </div>
             <?php endforeach; ?>
         </div>
@@ -82,17 +79,38 @@
         $(document).ready(function() {
             'use strict';
 
-            $('.js_print').on('click', function() {
-                var container = $(this).closest('.js_barcode_container');
-                var w = $('[name="w"]', container).val();
-                var h = $('[name="h"]', container).val();
-                var left = $('[name="left"]', container).val();
-                var top = $('[name="top"]', container).val();
-                var type = container.data('type');
-                var value = container.data('value');
-                var pars = '&w=' + w + '&h=' + h + '&left=' + left + '&top=' + top;
-                var url = '<?php echo base_url(); ?>' + container.data('url') + type + '/?val=' + value + pars;
-                window.open(url, '_blank');
+            $('.btn-view').on('click', function() {
+                var barcode_ct = $('.js_barcode_container', $('.js_barcode_grid'));
+
+                barcode_ct.find('img.barcode_img').prop('src', '');
+                barcode_ct.find('small.barcode_label').text('');
+                barcode_ct.prop('data-value', '');
+
+                var btn = $(this);
+
+                var barcode = btn.data('barcode');
+                var barcode_b64 = btn.data('barcode_b64');
+                var barcode_img = btn.data('barcode_img');
+
+
+                barcode_ct.find('img.barcode_img').prop('src', barcode_img);
+                barcode_ct.find('small.barcode_label').text(barcode);
+                barcode_ct.attr('data-value', barcode_b64);
+
+                barcode_ct.removeClass('hide');
+
+                $('.js_print').on('click', function() {
+                    var container = $(this).closest('.js_barcode_container');
+                    var w = $('[name="w"]', container).val();
+                    var h = $('[name="h"]', container).val();
+                    var left = $('[name="left"]', container).val();
+                    var top = $('[name="top"]', container).val();
+                    var type = container.data('type');
+                    var value = container.attr('data-value');
+                    var pars = '&w=' + w + '&h=' + h + '&left=' + left + '&top=' + top;
+                    var url = '<?php echo base_url(); ?>' + container.data('url') + type + '/?val=' + value + pars;
+                    window.open(url, '_blank');
+                });
             });
         });
     </script>
