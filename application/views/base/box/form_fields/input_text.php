52847a612160ddcba339441e2e96c4bd28090b5e
Michael <michael@h2web.it>
2022-08-10 10:31:44 +0200

    Fix undefined index input_text.php

--- application/views/base/box/form_fields/input_text.php
+++ application/views/base/box/form_fields/input_text.php
@@ -1,5 +1,5 @@
 <?php echo $label; ?>
-<input type="text" name="<?php echo $field['fields_name']; ?>" class="form-control <?php echo $class ?>" placeholder="<?php echo $placeholder; ?>" value="<?php echo $value; ?>" <?php echo $onclick; ?> autocomplete="off" <?php echo $attr; ?> data-dependent_on="<?php echo $field['forms_fields_dependent_on']; ?>" />
+<input type="text" name="<?php echo $field['fields_name']; ?>" class="form-control <?php echo $class ?>" placeholder="<?php echo $placeholder; ?>" value="<?php echo $value; ?>" <?php echo $onclick; ?> autocomplete="off" <?php echo $attr; ?> data-dependent_on="<?php echo (!empty($field['forms_fields_dependent_on'])) ? $field['forms_fields_dependent_on'] : null; ?>" />
 <?php if (!empty($field['support_data'])) : ?>
     <div class="help-block">
         <strong><?php e('Accepted values:') ?></strong>
