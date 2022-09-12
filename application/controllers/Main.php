59f37233036b498fce3dabb62a5839653d007417
Alessandro Ceccon <alessandro.ceccon@gmail.com>
2022-09-12 12:59:28 +0200

    Add initialize_permissions

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -872,4 +872,41 @@ class Main extends MY_Controller
             fpassthru($fp);
         }
     }
+    public function initialize_permissions()
+    {
+        $tipologia = $this->db->query("SELECT DISTINCT users_type FROM users")->result_array();
+        $dashobard = $this->apilib->searchFirst('layouts', ['layouts_dashboardable' => 1]);
+            foreach ($tipologia as $tipologia_user) {
+                if ($tipologia_user) {
+                    $user_type = $tipologia_user['users_type'];
+                    if($user_type){
+                        $tipologia = $this->apilib->view('users_type', $user_type);
+                        $permissions = $this->db->get_where('permissions',array('permissions_group' => $tipologia['users_type_value']));
+                        if ($permissions->num_rows() == 0) {
+                            //non c'è in permission, quindi vado a creare la riga
+                            $dati_db['permissions_admin']= 0;
+                            $dati_db['permissions_group']= $tipologia['users_type_value'];
+                            //per ogni utente che ha quel permesso, lo vado ad impostare
+                            $users = $this->db->query("SELECT users_id FROM users WHERE users_type = '$user_type'")->result_array();
+                            foreach ($users as $user) {
+                                if($user['users_id']){
+                                    $dati_db['permissions_user_id']= $user['users_id'];
+                                    $this->db->insert('permissions', $dati_db);
+                                    //ora imposto i permessi su 0 a tutto, a parte la dashboard iniziale
+                                    $dati_permissions['unallowed_layouts_user']= $user['users_id'];
+                                    $layouts = $this->db->query("SELECT layouts_id FROM layouts")->result_array();
+                                    foreach ($layouts as $layout) {
+                                        //escludo il primo dashboard layout
+                                        if ($layouts_id != $dashobard['layouts_id']) {
+                                            $dati_permissions['unallowed_layouts_layout']= $layout['layouts_id'];
+                                            $this->db->insert('unallowed_layouts', $dati_permissions);
+                                        }
+                                    }
+                                }
+                            }
+                        } 
+                    }
+                }
+            }
+    }
 }
