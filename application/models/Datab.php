0b0d53247ae317df45b8c8c920211d0edbe7665b
Matteo Puppis <matteopuppis@gmail.com>
2022-11-16 19:01:49 +0100

    Bugfix load module view custom

--- application/models/Datab.php
+++ application/models/Datab.php
@@ -3255,12 +3255,22 @@ class Datab extends CI_Model
                 $data = $this->get_menu($contentRef);
                 return $this->load->view("pages/layouts/menu/{$contentType}", array('data' => $data, 'value_id' => $value_id, 'layout_data_detail' => $layoutEntityData), true);
             case "view":
-                //Verifico se questa custom view fa parte di un modulo. In tal caso, carico la view direttamente dal modulo
-                if ($module_view = $this->getModuleViewData($contentRef)) {
-                    return $this->load->module_view($module_view['module_name'] . '/views', $module_view['module_view'], ['value_id' => $value_id, 'layout_data_detail' => $layoutEntityData], true);
+                //TODO: verificare prima se esiste un custom per questo modulo nelle view native custom
+                $module_view = $this->getModuleViewData($contentRef);
+
+                if (file_exists(FCPATH . "application/views/custom/{$module_view['module_name']}/{$module_view['module_view']}") || file_exists(FCPATH . "application/views/custom/{$module_view['module_name']}/{$module_view['module_view']}.php")) {
+                    return $this->load->view("custom/{$module_view['module_name']}/{$module_view['module_view']}", ['value_id' => $value_id, 'layout_data_detail' => $layoutEntityData], true);
+
                 } else {
-                    return $this->loadCustomView($contentRef, ['value_id' => $value_id, 'layout_data_detail' => $layoutEntityData], true);
+//Verifico se questa custom view fa parte di un modulo. In tal caso, carico la view direttamente dal modulo
+                    if ($module_view) {
+                        return $this->load->module_view($module_view['module_name'] . '/views', $module_view['module_view'], ['value_id' => $value_id, 'layout_data_detail' => $layoutEntityData], true);
+                    } else {
+                        return $this->loadCustomView($contentRef, ['value_id' => $value_id, 'layout_data_detail' => $layoutEntityData], true);
+                    }
+
                 }
+
             // no break
             default:
                 if (empty($contentType) && $layoutBoxData['layouts_boxes_content']) {
