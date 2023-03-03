00169037c19d8294a7bedd97b69d133a6a880a7f
Andrea <andrea@h2web.it>
2023-03-03 18:00:45 +0100

    Changed layout_unaccessible layout

--- application/views/base/pages/layout_unaccessible.php
+++ application/views/base/pages/layout_unaccessible.php
@@ -1,27 +1,36 @@
 <style>
 .container-border {
-    border: 2px solid #c7ccd0;
-    border-radius: 6px;
+    border: 2px solid #ccc;
+    border-radius: 20px;
     background-color: white;
 }
 
-.warning_sign_container {
-    display: flex;
-    justify-content: center;
+.row-margins {
+    margin-top: 50px;
+    margin-left: 200px;
+    margin-right: 200px;
+}
+
+@media screen and (max-width: 992px) {
+    .row-margins {
+        margin-top: 50px;
+        margin-left: 50px;
+        margin-right: 50px;
+    }
 }
 </style>
 
 
-<div class="container">
+<div class="container margin-x-lg">
     <div class="row row-margins container-border">
-        <div class="col-md-12">
-            <div class="warning_sign_container">
-                <img src="<?php echo base_url("images/warning_sign.png"); ?>" />
-            </div>
+        <div class="col-md-12 text-center">
+            <img src="<?php echo base_url("images/warning_sign.png"); ?>" />
         </div>
         <div class="col-md-12 text-center">
-            <h3><?php e('Access Denied'); ?></h3>
-            <p class="lead"><?php e('The page you requested does not exist'); ?> <br /> <?php e('or you don\'t have the right permissions.'); ?></p>
+            <h3>
+                <?php e('Access Denied');?>
+            </h3>
+            <p class="lead"><?php e('The page you requested does not exist'); ?> <br /><?php e('or you don\'t have the right permissions.'); ?></p>
         </div>
     </div>
 </div>
