519e75fa99bd67e382a31617fb4014c6d2fb68c7
matteo <>
2023-03-09 15:08:46 +0100

    Commit new release

--- application/config/constants.php
+++ application/config/constants.php
@@ -15,7 +15,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 defined('SHOW_DEBUG_BACKTRACE') or define('SHOW_DEBUG_BACKTRACE', true);
 
 // Client Version
-defined('VERSION') OR define('VERSION', '2.6.8');
+defined('VERSION') OR define('VERSION', '2.6.9');
 /*
 |--------------------------------------------------------------------------
 | File and Directory Modes
