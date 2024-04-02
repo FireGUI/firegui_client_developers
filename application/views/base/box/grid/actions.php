c4fef22d2935414f6802c619f8432280209b2f71
Alessandro Ceccon <alessandro.ceccon@gmail.com>
2024-04-02 10:40:38 +0200

    dropdown-menu

--- application/views/base/box/grid/actions.php
+++ application/views/base/box/grid/actions.php
@@ -1,11 +1,21 @@
 <div class="action-list">
+<style>
+    .dropdown-menu-custom {
+      left: auto;
+      right: 0;
+      min-width:100px;
+      padding-left:5px;
+      padding-right:5px;
+      box-shadow: 0 2px 2px 0 rgb(175 175 175 / 30%), 0 1px 3px 1px rgb(70 70 70 / 35%);
+    }
+  </style>
     <?php if (isset($links['custom']) && $links['custom']): ?>
         <?php
         //debug($links,true);
 // Filtra tutti i valori array e oggetto dall'array dati
         $row_data = array_filter(isset($row_data) ? $row_data : array(), function ($value) {
             return !is_array($value) && !is_object($value);
-        });
+        }); 
         $replace_from = empty($row_data) ? array() : array_map(function ($data) {
             return '{' . $data . '}';
         }, array_keys($row_data));
@@ -13,6 +23,84 @@
 
         $replace_from[] = '{value_id}';
         $replace_to[] = $id;
+        $dropdownExists = array_search('dropdown', array_column($links['custom'], 'grids_actions_show')) !== false;
+        $dropdownCount = 0;
+        $count_dropdown_show = 0;
+        if ($dropdownExists) {
+            $sortedLinks = [];
+            $dropdownLinks = [];
+            foreach ($links['custom'] as $link) {
+                if ($link['grids_actions_show'] === 'dropdown') {
+                    $dropdownLinks[] = $link;
+                    $dropdownCount++;
+                } else {
+                    $sortedLinks[] = $link;
+                }
+            }
+            // Unisci gli elementi con dropdown all'inizio dell'array
+            $links['custom'] = array_merge($dropdownLinks, $sortedLinks);
+        }
+        
+        if (!function_exists('createUrl')) { //validate if the function exists
+            function createUrl($custom_action, $id, $grid){
+                $confirm = false;
+                    if (empty($custom_action['grids_actions_link'])) {
+                        if (!empty($custom_action['grids_actions_type']) && in_array($custom_action['grids_actions_type'], ['detail', 'edit_layout'])) {
+                            $url = "{base_url}main/layout/{$custom_action['grids_actions_layout']}/$id";
+                            if ('modal' == $custom_action['grids_actions_mode']) {
+                                $url = "{base_url}get_ajax/layout_modal/{$custom_action['grids_actions_layout']}/$id";
+                            } elseif ('modal_large' == $custom_action['grids_actions_mode']) {
+                                $url = "{base_url}get_ajax/layout_modal/{$custom_action['grids_actions_layout']}/$id?_size=large";
+                            } elseif ('modal_extra' == $custom_action['grids_actions_mode']) {
+                                $url = "{base_url}get_ajax/layout_modal/{$custom_action['grids_actions_layout']}/$id?_size=extra";
+                            } elseif ('side_view' == $custom_action['grids_actions_mode']) {
+                                $url = "{base_url}get_ajax/layout_modal/{$custom_action['grids_actions_layout']}/$id?_mode=side_view";
+                            }
+                        } elseif (!empty($custom_action['grids_actions_type']) && 'delete' == $custom_action['grids_actions_type']) {
+                            $confirm = true;
+                            if (!empty($grid['grids']['entity_name'])) {
+                                $url = "{base_url}db_ajax/generic_delete/{$grid['grids']['entity_name']}/$id";
+                            } else {
+                                $url = "{base_url}db_ajax/generic_delete/{$grid['entity_name']}/$id";
+                            }
+                        } elseif (!empty($custom_action['grids_actions_type']) && 'duplicate_edit' == $custom_action['grids_actions_type']) {
+                            $url = "{base_url}main/form/{$custom_action['grids_actions_form']}/$id/true";
+                            if ('modal' == $custom_action['grids_actions_mode']) {
+                                $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id/true";
+                            } elseif ('modal_large' == $custom_action['grids_actions_mode']) {
+                                $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id/true?_size=large";
+                            } elseif ('modal_extra' == $custom_action['grids_actions_mode']) {
+                                $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id/true?_size=extra";
+                            } elseif ('side_view' == $custom_action['grids_actions_mode']) {
+                                $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id?_mode=side_view";
+                            }
+                        } elseif (!empty($custom_action['grids_actions_type']) && 'edit_form' == $custom_action['grids_actions_type']) {
+                            $url = "{base_url}main/form/{$custom_action['grids_actions_form']}/$id";
+                            if ('modal' == $custom_action['grids_actions_mode']) {
+                                $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id";
+                            } elseif ('modal_large' == $custom_action['grids_actions_mode']) {
+                                $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id?_size=large";
+                            } elseif ('modal_extra' == $custom_action['grids_actions_mode']) {
+                                $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id?_size=extra";
+                            } elseif ('side_view' == $custom_action['grids_actions_mode']) {
+                                $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id?_mode=side_view";
+                            }
+                        } else {
+                            $url = '';
+                        }
+                    } else {
+                        if (stripos($custom_action['grids_actions_link'], '{value_id}')) {
+                            $url = str_ireplace('{value_id}', $id, $custom_action['grids_actions_link']);
+                        } else {
+                            $url = "{$custom_action['grids_actions_link']}/$id";
+                        }
+                    }
+    
+                    $url = str_replace('{base_url}', base_url(), $url);
+                    return array($url, $confirm); // return both URL and confirm as an array
+                }
+        }
+
         ?>
         <?php foreach ($links['custom'] as $custom_action): ?>
             <?php if (!$this->conditions->accessible('grids_actions', $custom_action['grids_actions_id'], $id, $row_data)) {
@@ -22,7 +110,19 @@
                 continue;
             }
             ?>
-
+            <?php
+            if($custom_action['grids_actions_show'] == 'dropdown' AND $count_dropdown_show == 0){
+                echo "<div class='dropdown' style='display:inline;'>";
+                    echo "<button class='btn btn-sm btn-primary dropdown-toggle' type='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>";
+                    e("Actions");
+                    echo ' <span class="caret"></span>';
+                    echo "</button>";
+                    echo '<ul class="dropdown-menu dropdown-menu-left dropdown-menu-custom">';
+            }
+            if($custom_action['grids_actions_show'] == 'dropdown'){
+                $count_dropdown_show++;             
+            }
+            ?>
             <?php if (!empty($custom_action['grids_actions_html']) && (empty($custom_action['grids_actions_type']) || 'custom' == $custom_action['grids_actions_type'])): ?>
                 <?php
                 ob_start();
@@ -38,66 +138,12 @@
             <?php else: ?>
 
                 <?php
-                $confirm = false;
-                if (empty($custom_action['grids_actions_link'])) {
-                    if (!empty($custom_action['grids_actions_type']) && in_array($custom_action['grids_actions_type'], ['detail', 'edit_layout'])) {
-                        $url = "{base_url}main/layout/{$custom_action['grids_actions_layout']}/$id";
-                        if ('modal' == $custom_action['grids_actions_mode']) {
-                            $url = "{base_url}get_ajax/layout_modal/{$custom_action['grids_actions_layout']}/$id";
-                        } elseif ('modal_large' == $custom_action['grids_actions_mode']) {
-                            $url = "{base_url}get_ajax/layout_modal/{$custom_action['grids_actions_layout']}/$id?_size=large";
-                        } elseif ('modal_extra' == $custom_action['grids_actions_mode']) {
-                            $url = "{base_url}get_ajax/layout_modal/{$custom_action['grids_actions_layout']}/$id?_size=extra";
-                        } elseif ('side_view' == $custom_action['grids_actions_mode']) {
-                            $url = "{base_url}get_ajax/layout_modal/{$custom_action['grids_actions_layout']}/$id?_mode=side_view";
-                        }
-                    } elseif (!empty($custom_action['grids_actions_type']) && 'delete' == $custom_action['grids_actions_type']) {
-                        $confirm = true;
-                        if (!empty($grid['grids']['entity_name'])) {
-                            $url = "{base_url}db_ajax/generic_delete/{$grid['grids']['entity_name']}/$id";
-                        } else {
-                            $url = "{base_url}db_ajax/generic_delete/{$grid['entity_name']}/$id";
-                        }
-                    } elseif (!empty($custom_action['grids_actions_type']) && 'duplicate_edit' == $custom_action['grids_actions_type']) {
-                        $url = "{base_url}main/form/{$custom_action['grids_actions_form']}/$id/true";
-                        if ('modal' == $custom_action['grids_actions_mode']) {
-                            $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id/true";
-                        } elseif ('modal_large' == $custom_action['grids_actions_mode']) {
-                            $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id/true?_size=large";
-                        } elseif ('modal_extra' == $custom_action['grids_actions_mode']) {
-                            $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id/true?_size=extra";
-                        } elseif ('side_view' == $custom_action['grids_actions_mode']) {
-                            $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id?_mode=side_view";
-                        }
-                    } elseif (!empty($custom_action['grids_actions_type']) && 'edit_form' == $custom_action['grids_actions_type']) {
-                        $url = "{base_url}main/form/{$custom_action['grids_actions_form']}/$id";
-                        if ('modal' == $custom_action['grids_actions_mode']) {
-                            $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id";
-                        } elseif ('modal_large' == $custom_action['grids_actions_mode']) {
-                            $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id?_size=large";
-                        } elseif ('modal_extra' == $custom_action['grids_actions_mode']) {
-                            $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id?_size=extra";
-                        } elseif ('side_view' == $custom_action['grids_actions_mode']) {
-                            $url = "{base_url}get_ajax/modal_form/{$custom_action['grids_actions_form']}/$id?_mode=side_view";
-                        }
-                    } else {
-                        $url = '';
-                    }
-                } else {
-                    if (stripos($custom_action['grids_actions_link'], '{value_id}')) {
-                        $url = str_ireplace('{value_id}', $id, $custom_action['grids_actions_link']);
-                    } else {
-                        $url = "{$custom_action['grids_actions_link']}/$id";
-                    }
-                }
-
-                $url = str_replace('{base_url}', base_url(), $url);
+                list($url, $confirm) = createUrl($custom_action, $id, $grid);
                 //debug($custom_action);
-    
                 ?>
                 <span <?php echo $custom_action['grids_actions_name'] ? "data-toggle='tooltip' title='" . t($custom_action['grids_actions_name']) . "'" : null; ?>>
                     <a class="js-action_button btn btn-grid-action-s
-        bg<?php echo strtoupper(rgb_string_to_hex($custom_action['grids_actions_color'])) ?: 'CCCCCC'; ?>
+                    bg<?php echo strtoupper(rgb_string_to_hex($custom_action['grids_actions_color'])) ?: 'CCCCCC'; ?>
                     <?php if ($confirm): ?> js_confirm_button js_link_ajax<?php endif; ?>
                     <?php if (in_array($custom_action['grids_actions_mode'], ['modal', 'modal_large', 'modal_extra', 'side_view'])): ?> js_open_modal <?php endif; ?>"
                         href="<?php echo $url; ?>" <?php if ($custom_action['grids_actions_mode'] == 'new_tab'): ?>target="_blank"
@@ -109,7 +155,12 @@
                 </span>
 
             <?php endif; ?>
-
+            <?php
+            if($custom_action['grids_actions_show'] == 'dropdown' AND $count_dropdown_show == $dropdownCount){
+                echo "</ul>";
+                echo "</div>";
+            }
+            ?>
         <?php endforeach; ?>
     <?php endif; ?>
 
