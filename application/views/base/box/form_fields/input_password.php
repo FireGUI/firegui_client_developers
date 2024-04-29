ddf8827470ed1872f735df87e27224922415ebae
Michael <michael@h2web.it>
2024-04-29 15:15:09 +0200

    added autocomplete deactivation of browser password field

--- application/views/base/box/form_fields/input_password.php
+++ application/views/base/box/form_fields/input_password.php
@@ -1,6 +1,6 @@
 <?php echo $label; ?>
 <div class="input-group">
-    <input type="password" name="<?php echo $field['fields_name']; ?>" class="form-control <?php echo $class ?>" placeholder="<?php echo $placeholder; ?>" <?php echo $onclick; ?> data-dependent_on="<?php echo $field['forms_fields_dependent_on']; ?>" />
+    <input type="password" name="<?php echo $field['fields_name']; ?>" class="form-control <?php echo $class ?>" placeholder="<?php echo $placeholder; ?>" <?php echo $onclick; ?> data-dependent_on="<?php echo $field['forms_fields_dependent_on']; ?>" autocomplete="new-password"  />
     <span class="input-group-addon js_show_user_password">
         <i class="fas fa-eye password-icon"></i>
     </span>
