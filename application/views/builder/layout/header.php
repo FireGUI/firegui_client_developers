365c557e0fb0773ea314a515a8adc05753088bfa
Michael <michael@h2web.it>
2024-09-19 10:38:50 +0200

    fix profile image old imgn

--- application/views/builder/layout/header.php
+++ application/views/builder/layout/header.php
@@ -132,7 +132,7 @@ $this->layout->addDinamicStylesheet($data, "header.css");
             <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                     <?php
-                    $_img = ($this->config->item('cdn') && $this->config->item('cdn')['enabled']) ? base_url_admin("uploads/" . $this->auth->get(LOGIN_IMG_FIELD)) : base_url_admin("imgn/1/100/100/uploads/" . $this->auth->get(LOGIN_IMG_FIELD));
+                    $_img = ($this->config->item('cdn') && $this->config->item('cdn')['enabled']) ? base_url_admin("uploads/" . $this->auth->get(LOGIN_IMG_FIELD)) : base_url_admin("thumb/1/100/100/uploads/" . $this->auth->get(LOGIN_IMG_FIELD));
                     ?>
 
                     <img src="<?php echo ($this->auth->get(LOGIN_IMG_FIELD) ? $_img : base_url_admin('images/user.png')); ?>"
