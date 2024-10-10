92c5a7015c522771448cf1803c2a45f5dfe1352c
Matteo Puppis <matteopuppis@gmail.com>
2024-10-10 14:42:24 +0200

    Added migration for new htaccess

--- application/migrations/update_php_code.php
+++ application/migrations/update_php_code.php
@@ -109,6 +109,7 @@ $updates['4.0.2'] = [
 $updates['4.1.4'] = [
     'include' => [
         '4.1.4_field_user_verification_code',
-        '4.1.4_new_email_templates'
+        '4.1.4_new_email_templates',
+        '4.1.4_add_htaccess_public_rule.php',
     ]
 ];
