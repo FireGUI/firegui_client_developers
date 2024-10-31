b2b33b441fde7538fd4f16fc6394c50fc44b8ca7
Matteo Puppis <matteopuppis@gmail.com>
2024-10-31 12:21:28 +0100

    Bugfix undefined fields_ref Removed migration 4.2.0 (to be scheduled)

--- application/migrations/update_php_code.php
+++ application/migrations/update_php_code.php
@@ -133,8 +133,8 @@ $updates['4.1.7'] = [
     ]
 ];
 
-$updates['4.1.9'] = [
-    'include' => [
-        '4.1.9_add_currencies_system_entity.php',
-    ]
-];
+// $updates['4.2.0'] = [
+//     'include' => [
+//         '4.2.0_add_currencies_system_entity.php',
+//     ]
+// ];
