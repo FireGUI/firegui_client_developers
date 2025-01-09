3c0baed7cd8bedcaf0f82c57d8ecdc9c80207c13
Michael <michael@h2web.it>
2025-01-09 10:38:59 +0100

    aggiunto controllo esistenza campo settings_default_currency

--- application/models/Datab.php
+++ application/models/Datab.php
@@ -50,7 +50,7 @@ class Datab extends CI_Model
             $this->db->join('settings_template', 'settings.settings_template = settings_template.settings_template_id', 'LEFT');
         }
         
-        if ($this->db->table_exists('currencies')) {
+        if ($this->db->table_exists('currencies') && $this->db->field_exists('settings_default_currency', 'settings')) {
             $this->db->join('currencies', 'settings.settings_default_currency = currencies.currencies_id', 'LEFT');
             
         }
